9040c572209265895aa2f67fb4e1f079a5e44122
Jano at Zelda <alejandro@mosteo.com>
2018-02-02 19:29:22 +0100

    Towards bootstrapper

--- alr-bootstrap.sh
+++ alr-bootstrap.sh
@@ -3,10 +3,48 @@
 set -o errexit
 set -o nounset
 
+ensure_cmd() {
+    command -v $1 >/dev/null || { 
+        cat <<EOF
+A required command is not in the session PATH.
+Please install it and retry.
+
+Missing command: $1
+EOF
+    exit 1
+    }
+}
+
+check_no_root() {
+    if [[ `id -u` == 0 ]]; then
+        echo This script must not be run as root
+        exit 1
+    fi
+}
+
+check_ada_compiler() {
+    return
+}
+
+function install_linux() {
+    for cmd in git id; do
+        ensure_cmd $cmd
+    done
+
+    check_no_root
+    check_ada_compiler
+}
+
+function main() {
+
 case $OSTYPE in
     linux-gnu)
-        os=linux;;
+        install_linux;;
     *)
         echo Unsupported platform: "[$OSTYPE]"
         exit 1;;
 esac
+
+}
+
+main "$@"
