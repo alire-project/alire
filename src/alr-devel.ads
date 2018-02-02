77f077ee469c1e93b814a3f15f1e3e21a100a393
Jano at Zelda <alejandro@mosteo.com>
2018-02-02 18:44:53 +0100

    Disabled Devel mode

--- src/alr-devel.ads
+++ src/alr-devel.ads
@@ -3,6 +3,6 @@ package Alr.Devel with Preelaborate is
    Enabled : constant Boolean := False;
    --  This global switch changes some behaviors to make for a more friendlier development experience:
    --  Rolling behavior is disabled so there's never respawning of the canonical rolling executable
-   --  Self-compilation is launched for the project file in the current folder instead of the canonical one
+   --  Self-compilation is launched for the project file in the hard-coded devel folder instead of the canonical one
 
 end Alr.Devel;
