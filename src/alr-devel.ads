8fa18115025d8b178fddcb65d9cc13d1346f3f21
Alejandro R. Mosteo <alejandro@mosteo.com>
2018-02-05 09:59:30 +0100

    Revoked Devel flag

--- src/alr-devel.ads
+++ src/alr-devel.ads
@@ -1,6 +1,6 @@
 package Alr.Devel with Preelaborate is
 
-   Enabled : constant Boolean := True;
+   Enabled : constant Boolean := False;
    --  This global switch changes some behaviors to make for a more friendlier development experience:
    --  Rolling behavior is disabled so there's never respawning of the canonical rolling executable
    --  Self-compilation is launched for the project file in the hard-coded devel folder instead of the canonical one
