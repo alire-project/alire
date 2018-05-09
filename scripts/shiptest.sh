b04d668ad167aab70975f14c41752fe4bf38b4fb
Alejandro R Mosteo <alejandro@mosteo.com>
2018-05-09 17:41:42 +0200

    Test installer on master commits

---
+++ scripts/shiptest.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+trap 'echo "ERROR at line ${LINENO} (code: $?)" >&2' ERR 
+trap 'echo "Interrupted" >&2 ; exit 1' INT 
+
+set -o errexit
+set -o nounset
+
+if [ "$BRANCH" == "master" ]; then 
+    echo -e '\n/bin\ny' | ./install/alr_bootstrap.sh
+    alr update --online
+else
+    gprbuild -p -P alr_env
+fi 
+
+export PATH+=:`pwd`/bin
+
+alr search --list --native
+
+bash stress/selftest.sh docker
+
+mkdir reltest selftest
+cd selftest
+alr dev --test
+cp *.xml ../shippable/testresults
+cd ../reltest 
+alr test --full
+cp *.xml ../shippable/testresults 
