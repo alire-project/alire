af0f8fd30fd0d2cf0bc72805df85ca2d6ad8c929
Alejandro R Mosteo <alejandro@mosteo.com>
2018-05-22 16:44:04 +0200

    proper file extension

--- scripts/shippost.sh
+++ scripts/shippost.sh
@@ -2,9 +2,9 @@
 
 dst=`basename $IMAGE_NAME`
 if [ "`find reltest -name '*.txt' | wc -l`" -gt 0 ]; then
-    cp -fv reltest/*.txt status/$dst
+    cp -fv reltest/*.txt status/$dst.txt
 else
-    echo "alr test failed to run in $dst" > status/$dst
+    echo "alr test failed to run in $dst" > status/$dst.txt
 fi
 
 git add status
