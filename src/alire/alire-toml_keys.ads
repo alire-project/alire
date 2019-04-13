52d939a85e926b796f25295d10b5ddd7669f68da
Alejandro R. Mosteo <amosteo@unizar.es>
2019-04-13 13:04:15 +0200

    Alire.Releases: TOML export of labeled properties

---
+++ src/alire/alire-toml_keys.ads
@@ -0,0 +1,22 @@
+with Alire.Properties.Labeled;
+
+package Alire.TOML_Keys with Preelaborate is
+
+   --  Constants for TOML keys in the index format
+   --  TODO: update elsewere   
+   
+   use all type Properties.Labeled.Labels;   
+   function Labels (Label : Properties.Labeled.Labels) return String is      
+     (case Label is 
+         when Author       => "authors",
+         when Comment      => "comment",
+         when Description  => "description",
+         when Executable   => "executables",
+         when Maintainer   => "maintainers",
+         when Path         => "paths",
+         when Project_File => "project-files",
+         when Website      => "website");
+
+   Description : constant String := "description";
+   
+end Alire.TOML_Keys;
