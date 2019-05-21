8f68258a87ca79e8b9d81c73cabb5e32fe64bd99
Alejandro R. Mosteo <amosteo@unizar.es>
2019-05-21 18:30:25 +0200

    moved Spawn from Alr to Alire for future use

--- src/alire/alire-os_lib.ads
+++ src/alire/alire-os_lib.ads
@@ -3,6 +3,11 @@ package Alire.OS_Lib with Preelaborate is
    function "/" (L, R : String) return String;
    --  Shorthand for path composition
 
+   -- Package to enable easy use of "/"
+   package Operators is
+      function "/" (L, R : String) return String renames OS_Lib."/";
+   end Operators;
+
    procedure Bailout (Code : Integer := 0);
 
    function Exe_Suffix return String;
