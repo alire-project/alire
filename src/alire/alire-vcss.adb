cef4f596856480b075617ca04e3f900167392680
Alejandro R. Mosteo <amosteo@unizar.es>
2019-05-22 16:34:38 +0200

    Centralize calls to VCSs in Alire.VCSs

---
+++ src/alire/alire-vcss.adb
@@ -0,0 +1,57 @@
+with Alire.Utils;
+with Alire.VCSs.Git;
+
+package body Alire.VCSs is
+
+   -----------
+   -- Clone --
+   -----------
+
+   function Clone (From : URL;
+                   Into : Platform_Independent_Path)
+                   return Outcome
+   is
+   begin
+      case Kind (From) is
+         when VCS_Git =>
+            return Git.Handler.Clone (Repo_And_Commit (From), Into);
+         when VCS_Unknown =>
+            return Outcome_Failure ("Unknown VCS requested: " & From);
+      end case;
+   end Clone;
+
+   ------------
+   -- Commit --
+   ------------
+
+   function Commit (Origin : URL) return String is
+     (if Utils.Contains (Origin, "@")
+      then Utils.Tail (Origin, '@')
+      else "");
+
+   ----------
+   -- Kind --
+   ----------
+
+   function Kind (Origin : URL) return Kinds is
+     (if Utils.Starts_With (Origin, "git+") then VCS_Git
+      else VCS_Unknown);
+
+
+   ----------
+   -- Repo --
+   ----------
+
+   function Repo (Origin : URL) return String is
+     (Utils.Head (Repo_And_Commit (Origin), '@'));
+
+   ---------------------
+   -- Repo_And_Commit --
+   ---------------------
+
+   function Repo_And_Commit (Origin : URL) return String is
+     (if Utils.Contains (Origin, "+http")
+      then Utils.Tail (Origin, '+')
+      else Origin);
+
+end Alire.VCSs;
---
+++ src/alire/alire-vcss.ads
@@ -0,0 +1,43 @@
+package Alire.VCSs is
+
+   type VCS is interface;
+
+   type Kinds is (VCS_Git, VCS_Unknown);
+
+   subtype Known_Kinds is Kinds range Kinds'First .. Kinds'Pred (VCS_Unknown);
+
+   --  URL format:
+   --  vcs+http[s]://path/to/repo[@commit]
+
+   function Clone (This : VCS;
+                   From : URL;
+                   Into : Platform_Independent_Path)
+                   return Outcome is abstract;
+
+   function Update (This : VCS;
+                    Repo : Platform_Independent_Path)
+                    return Outcome is abstract;
+
+
+   --  General utilities
+
+   function Kind (Origin : URL) return Kinds;
+
+   function Repo (Origin : URL) return String;
+   --  Without kind and commit
+
+   function Repo_And_Commit (Origin : URL) return String;
+   --  Without Kind and with optional Commit
+
+   function Commit (Origin : URL) return String;
+   --  Empty string if no commit part
+
+
+   --  Classwide versions that redispatch to the appropriate descendant
+
+   function Clone (From : URL;
+                   Into : Platform_Independent_Path)
+                   return Outcome
+     with Pre => Kind (From) in Known_Kinds;
+
+end Alire.VCSs;
