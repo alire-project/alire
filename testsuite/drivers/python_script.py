46a744c9c4c5e4f8109ab7e909165d0bdae65851
Pierre-Marie de Rodat <derodat@adacore.com>
2019-06-18 14:08:17 +0200

    Add a testsuite framework
    For #5

---
+++ testsuite/drivers/python_script.py
@@ -0,0 +1,74 @@
+import os
+import sys
+
+from e3.fs import sync_tree
+from e3.testsuite import TestAbort
+from e3.testsuite.driver import TestDriver
+from e3.testsuite.process import check_call
+from e3.testsuite.result import TestStatus
+
+from drivers.alr import prepare_env, prepare_indexes
+
+
+class PythonScriptDriver(TestDriver):
+    """
+    Test driver to run a "test.py" Python script.
+
+    This test driver runs a Python script. For a testcase to succeeds, the
+    script expects it to exit with status code 0, its standard error stream to
+    be empty and its standard output stream to end with a line that contains
+    "SUCCESS". Anything else results in the test failing.
+    """
+
+    def add_test(self, dag):
+        self.add_fragment(dag, 'run')
+
+    def run(self, previous_values):
+        # Copy test material to the temporary directory
+        sync_tree(self.test_env['test_dir'],
+                  self.test_env['working_dir'])
+
+        # Prepare the environment so that the Python script can run "alr"
+        env = dict(os.environ)
+        config_dir = os.path.join(self.test_env['working_dir'], 'alr-config')
+        prepare_env(config_dir, env)
+        prepare_indexes(config_dir,
+                        self.test_env['working_dir'],
+                        self.test_env.get('indexes', {}))
+
+        # Also give it access to our Python helpers
+        python_path = env.get('PYTHONPATH', '')
+        path_for_drivers = os.path.abspath(
+            os.path.dirname(os.path.dirname(__file__)))
+        env['PYTHONPATH'] = '{}{}{}'.format(
+            path_for_drivers, os.path.pathsep, python_path
+        ) if python_path else path_for_drivers
+
+        # Run the Python script with the current interpreter. check_call aborts
+        # the test if the interpreter exits with non-zero status code.
+        p = check_call(self,
+                       [sys.executable, 'test.py'],
+                       env=env,
+                       cwd=self.test_env['working_dir'])
+
+        # Check that stderr is empty
+        if p.err:
+            self.result.set_status(TestStatus.FAIL, 'non-empty stderr')
+            self.result.log += 'non-empty stderr:\n'
+            self.result.log += p.err
+            self.push_result()
+            raise TestAbort
+
+        # Check that the last line in stdout is "SUCCESS"
+        out_lines = p.out.splitlines()
+        if not out_lines or out_lines[-1] != 'SUCCESS':
+            self.result.set_status(TestStatus.FAIL,
+                                   'missing SUCCESS output line')
+            self.result.log += 'missing SUCCESS output line'
+            self.push_result()
+            raise TestAbort
+
+        # If we reach this, the test succeeds
+        self.result.set_status(TestStatus.PASS)
+        self.push_result()
+        return True
---
+++ testsuite/run.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+
+"""
+e3.testsuite-based testsuite for Alire/ALR.
+
+Just execute this script to run the testsuite. It requires a Python2
+interpreter with the e3-core and e3-testsuite packages (from PyPI) installed.
+"""
+
+from __future__ import absolute_import, print_function
+
+import os.path
+
+import e3.testsuite
+import e3.testsuite.driver
+
+
+from drivers.python_script import PythonScriptDriver
+
+
+class Testsuite(e3.testsuite.Testsuite):
+    TEST_SUBDIR = 'tests'
+    DRIVERS = {'python-script': PythonScriptDriver}
+
+    @property
+    def default_driver(self):
+        return 'decoder'
+
+
+if __name__ == '__main__':
+    suite = Testsuite(os.path.dirname(__file__))
+    suite.testsuite_main()
+
+    # Display statistics about test results: number of tests per status
+    stats = [(str(name).split('.')[1], count)
+             for name, count in suite.test_status_counters.items()
+             if count]
+    for name, count in sorted(stats):
+        print('{: <8} {}'.format(name + ':', count))
