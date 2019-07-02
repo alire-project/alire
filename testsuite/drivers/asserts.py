a158f3c823aeed70f691a322304c6c428f055be3
Pierre-Marie de Rodat <derodat@adacore.com>
2019-07-02 18:01:27 +0200

    Testsuite: add the "assert_match" helper
    For #140

--- testsuite/drivers/asserts.py
+++ testsuite/drivers/asserts.py
@@ -3,6 +3,8 @@ This module provides several helpers to perform user-friendly assertions in
 testcases based on Python scripts.
 """
 
+import re
+
 
 def indent(text, prefix='  '):
     """
@@ -18,4 +20,14 @@ def assert_eq(expected, actual, label=None):
                 indent(str(expected)),
                 'But got:',
                 indent(str(actual))]
+        assert False, '\n'.join(text)
+
+
+def assert_match(expected_re, actual, label=None):
+    if not re.match(expected_re, actual):
+        text = ['Unexpected {}'.format(label or 'output'),
+                'Expecting a match on:',
+                indent(expected_re),
+                'But got:',
+                indent(actual)]
         assert False, '\n'.join(text)
