689fde83d1293bd44434dabe2f8ed4ca059e2e9a
Alejandro R. Mosteo <amosteo@unizar.es>
2019-07-12 12:37:16 +0200

    Alire: Separate flag for debug-specific output
    Also removed some dead code

--- src/alire/alire.adb
+++ src/alire/alire.adb
@@ -1,4 +1,17 @@
+with GNAT.IO;
+
 package body Alire is
+
+   -------------
+   -- Err_Log --
+   -------------
+   --  Write given string to Standard_Error
+
+   procedure Err_Log (S : String) is
+      use GNAT.IO;
+   begin
+      Put_Line (Standard_Error, "stderr: " & S);
+   end Err_Log;
 
    -------------------
    -- Log_Exception --
@@ -14,6 +27,12 @@ package body Alire is
       Log (Exception_Message (E), Level);
       Log (Exception_Information (E), Level);
       Log ("--->8--- Exception dump end ----->8---", Level);
+
+      if Log_Debug then
+         Err_Log (Exception_Name (E));
+         Err_Log (Exception_Message (E));
+         Err_Log (Exception_Information (E));
+      end if;
    end Log_Exception;
 
    ----------------------------
