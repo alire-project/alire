168a92587d77048ac2e33fec5c074abfc7a21f2c
Alejandro R. Mosteo <amosteo@unizar.es>
2019-07-12 18:39:05 +0200

    Test case for debug scopes and supporting changes

---
+++ testsuite/tests/debug/logging-scopes/test.py
@@ -0,0 +1,48 @@
+"""
+Test the fidelity of various logging scopes
+"""
+
+from glob import glob
+
+from drivers.alr import run_alr
+from drivers.asserts import assert_eq
+from drivers.asserts import assert_match
+
+# Check invalid syntax detection
+p = run_alr('dev', '-d--', complain_on_error=False, quiet=False)
+assert p.status == 1, "alr should have error code 1"
+assert_eq('Filtering mode: BLACKLIST\n'
+          'Invalid logging scope separator: --\n'
+          'ERROR: Invalid logging filters.',
+          p.out.strip())
+
+# Check empty whitelist lets nothing through
+p = run_alr('dev', '-vv', '-d+', quiet=False)
+assert_eq('Filtering mode: WHITELIST', p.out.strip())
+
+# Check whitelisting
+p = run_alr('dev', '--filter', '-vv', '-d+dev', quiet=False)
+assert_match('Filtering mode: WHITELIST\n'
+             'Filtering substring: dev\n'
+             '\[Alr.Commands.Dev.Execute\] \(alr-commands-dev.adb:[0-9]* \)'
+             ' -->> In dev --filter',
+             p.out.strip())
+
+# Check whitelisting with exception
+p = run_alr('dev', '--filter', '-vv', '-d+dev-exec', quiet=False)
+assert_eq('Filtering mode: WHITELIST\n'
+          'Filtering substring: dev\n'
+          'Filtering exception: exec',
+          p.out.strip())
+
+# Check blacklisting with exception
+p = run_alr('dev', '-vv', '-d-a+main', quiet=False)
+assert_match('Filtering mode: BLACKLIST\n'
+             'Filtering substring: a\n'
+             'Filtering exception: main\n'
+             '\[Alr.Main                \] \(alr-main.adb:.*',
+             # Remaining output is likely to change in the future so lets just
+             # stop here the matching.
+             p.out.strip())
+
+print('SUCCESS')
