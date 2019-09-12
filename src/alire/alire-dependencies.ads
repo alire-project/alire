16c006fa4626dd5ad55a0ed9dea942c94883c4ce
Alejandro R. Mosteo <amosteo@unizar.es>
2019-09-12 19:14:23 +0200

    Implement From_TOML for dependencies

--- src/alire/alire-dependencies.ads
+++ src/alire/alire-dependencies.ads
@@ -1,4 +1,5 @@
 with Alire.Interfaces;
+with Alire.TOML_Adapters;
 with Alire.Utils;
 
 with Semantic_Versioning;
@@ -30,6 +31,18 @@ package Alire.Dependencies with Preelaborate is
 
    overriding
    function Key (Dep : Dependency) return String;
+
+   function From_TOML (Key    : String;
+                       Value  : TOML.TOML_Value) return Dependency with
+     Pre =>
+       (Key'Length >= Min_Name_Length or else
+          raise Checked_Error with "dependency name too short") and then
+       (Value.Kind = TOML.TOML_String or else
+          raise Checked_Error with "dependency version must be a string");
+   --  May raise Checked_Error with stored Alire.Errors.
+
+   function From_TOML (From : TOML_Adapters.Key_Queue) return Dependency;
+   --  Wrapper for case loaders; may fail as the previous function.
 
    overriding
    function To_TOML (Dep : Dependency) return TOML.TOML_Value;
