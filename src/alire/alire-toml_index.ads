8cd5a31b5aae79d45ce3a9e28768b59af2e194ad
Alejandro R. Mosteo <amosteo@unizar.es>
2019-09-13 09:43:37 +0200

    Indexing of crates with new structures

--- src/alire/alire-toml_index.ads
+++ src/alire/alire-toml_index.ads
@@ -1,3 +1,6 @@
+private with TOML;
+
+with Alire.Projects.With_Releases;
 with Alire.Releases;
 with Alire.Requisites;
 with Alire.TOML_Adapters;
@@ -18,5 +21,22 @@ package Alire.TOML_Index is
    function Load_Release_From_File (Filename : String) return Releases.Release;
    --  Load a file that must contain a single release.
    --  May raise Checked_Error if Filename hasn't proper contents.
+
+private
+
+   function Fix_Release_Local_Origin_Path
+     (Crate_Path : Relative_Path;
+      Release    : Releases.Release) return Releases.Release;
+   --  Takes a freshly loaded Release with a relative path from its index file
+   --  to its actual crate location, and replaces it with the absolute path.
+   --  If Release has no local origin, does nothing.
+
+   procedure Index_Crate (Path  : Relative_Path;
+                          Crate : Projects.With_Releases.Crate);
+   --  Add the crate and its releases to the internal index.
+   --  Path is where on disk the Crate was loaded from. This is necessary
+   --  to fix relative paths in local origins, which at load time are relative
+   --  to the Crate location, but at runtime the current dir may be any other.
+   --  May raise Checked_Error if a release has an invalid path as origin.
 
 end Alire.TOML_Index;
