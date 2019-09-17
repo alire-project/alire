af3b0e704c8b1a9ed0ab3db56bf56849eef53b50
Alejandro R. Mosteo <amosteo@unizar.es>
2019-09-17 12:11:49 +0200

    Fix test to support new mandatory field

--- testsuite/tests/index/origin-no-archive-name/test.py
+++ testsuite/tests/index/origin-no-archive-name/test.py
@@ -5,11 +5,10 @@ Test that origins lacking archive names in indexes are properly reported.
 from drivers.alr import run_alr
 from drivers.asserts import assert_match
 
+import re
 
 p = run_alr('show', 'hello_world', complain_on_error=False, quiet=False)
-assert_match(
-    'ERROR:.*missing mandatory archive-name'
-    '\nERROR: alr show unsuccessful'
-    '\n', p.out)
+assert_match('.*unable to determine archive name from URL.*',
+             p.out, flags=re.S)
 
 print('SUCCESS')
