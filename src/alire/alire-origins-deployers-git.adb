09de55cb4fa4b16c5587716e937458fd33120444
Alejandro R. Mosteo <amosteo@unizar.es>
2019-10-17 18:18:25 +0200

    Remove git hashing code

--- src/alire/alire-origins-deployers-git.adb
+++ src/alire/alire-origins-deployers-git.adb
@@ -1,7 +1,3 @@
-with Alire.Directories;
-with Alire.Errors;
-with Alire.OS_Lib.Subprocess;
-
 with Alire.VCSs.Git;
 
 package body Alire.Origins.Deployers.Git is
@@ -15,38 +11,5 @@ package body Alire.Origins.Deployers.Git is
    begin
       return VCSs.Git.Handler.Clone (This.Base.URL_With_Commit, Folder);
    end Deploy;
-
-   ------------------
-   -- Compute_Hash --
-   ------------------
-
-   overriding
-   function Compute_Hash (This   : Deployer;
-                          Folder : String;
-                          Kind   : Hashes.Kinds) return Hashes.Any_Digest
-   is
-      pragma Unreferenced (This);
-      use OS_Lib.Subprocess;
-
-      --  Enter the folder to hash
-      Guard : Directories.Guard (Directories.Enter (Folder)) with unreferenced;
-
-      Output    : Utils.String_Vector;
-      Tmp_File  : Directories.Temp_File;
-
-      --  Generate platform-independent archive
-      Exit_Code : constant Integer :=
-                    Spawn ("git",
-                           "-c core.autocrlf=false archive HEAD -o "
-                             & Tmp_File.Filename);
-   begin
-      if Exit_Code /= 0 then
-         raise Checked_Error with Errors.Set
-           ("Unexpected error while executing process:" & Exit_Code'Img
-            & "; output: " & Output.Flatten);
-      else -- OK
-         return Hashes.Digest (Hashes.Hash_File (Kind, Tmp_File.Filename));
-      end if;
-   end Compute_Hash;
 
 end Alire.Origins.Deployers.Git;
