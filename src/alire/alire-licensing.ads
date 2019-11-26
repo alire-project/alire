c4277504b9a706c3f731636c0d510867080ab1c8
Fabien Chouteau <fabien.chouteau@gmail.com>
2019-11-27 00:23:58 +0100

    Licenses: remove the possibility of custom license text
    As discussed in #191, it is enough to limit the license field to one of
    the possible identifier supported.

--- src/alire/alire-licensing.ads
+++ src/alire/alire-licensing.ads
@@ -1,5 +1,3 @@
-with Ada.Strings.Unbounded;
-
 package Alire.Licensing with Preelaborate is
 
    --  From https://github.com/github/choosealicense.com
@@ -42,51 +40,53 @@ package Alire.Licensing with Preelaborate is
                      GMGPL_2_0,
                      GMGPL_3_0,
                      Public_Domain,
+                     Custom,
                      Unknown);
 
    function License_Labels (L : Licenses)
-                            return Ada.Strings.Unbounded.Unbounded_String
+                            return String
    is
      (case L is
-      when AFL_3_0              => +"AFL 3.0",
-      when AGPL_3_0             => +"AGPL 3.0",
-      when Apache_2_0           => +"Apache 2.0",
-      when Artistic_2_0         => +"Artistic 2.0",
-      when BSD_2_Clause         => +"BSD 2-Clauses",
-      when BSD_3_Clause_Clear   => +"BSD 3-Clauses Clear",
-      when BSD_3_Clause         => +"BSD 3-Clauses",
-      when BSL_1_0              => +"BSL 1.0",
-      when CC0_1_0              => +"CC0 1.0",
-      when CC_BY_4_0            => +"CC BY 4.0",
-      when CC_BY_SA_4_0         => +"CC BY-SA 4.0",
-      when ECL_2_0              => +"ECL 2.0",
-      when EPL_1_0              => +"EPL 1.0",
-      when EPL_2_0              => +"EPL 2.0",
-      when EUPL_1_1             => +"EUPL 1.1",
-      when EUPL_1_2             => +"EUPL 1.2",
-      when GPL_2_0              => +"GPL 2.0",
-      when GPL_3_0              => +"GPL 3.0",
-      when ISC                  => +"ISC",
-      when LGPL_2_1             => +"LGPL 2.1",
-      when LGPL_3_0             => +"LGPL 3.0",
-      when LPPL_1_3c            => +"LPPL 1.3c",
-      when MIT                  => +"MIT",
-      when MPL_2_0              => +"MPL 2.0",
-      when MS_PL                => +"MS PL",
-      when MS_RL                => +"MS RL",
-      when NCSA                 => +"NCSA",
-      when OFL_1_1              => +"OFL 1.1",
-      when OSL_3_0              => +"OSL 3.0",
-      when PostgreSQL           => +"PostgreSQL",
-      when Unlicense            => +"Unlicense",
-      when WTFPL                => +"WTFPL",
-      when Zlib                 => +"zlib",
+      when AFL_3_0              => "AFL 3.0",
+      when AGPL_3_0             => "AGPL 3.0",
+      when Apache_2_0           => "Apache 2.0",
+      when Artistic_2_0         => "Artistic 2.0",
+      when BSD_2_Clause         => "BSD 2-Clauses",
+      when BSD_3_Clause_Clear   => "BSD 3-Clauses Clear",
+      when BSD_3_Clause         => "BSD 3-Clauses",
+      when BSL_1_0              => "BSL 1.0",
+      when CC0_1_0              => "CC0 1.0",
+      when CC_BY_4_0            => "CC BY 4.0",
+      when CC_BY_SA_4_0         => "CC BY-SA 4.0",
+      when ECL_2_0              => "ECL 2.0",
+      when EPL_1_0              => "EPL 1.0",
+      when EPL_2_0              => "EPL 2.0",
+      when EUPL_1_1             => "EUPL 1.1",
+      when EUPL_1_2             => "EUPL 1.2",
+      when GPL_2_0              => "GPL 2.0",
+      when GPL_3_0              => "GPL 3.0",
+      when ISC                  => "ISC",
+      when LGPL_2_1             => "LGPL 2.1",
+      when LGPL_3_0             => "LGPL 3.0",
+      when LPPL_1_3c            => "LPPL 1.3c",
+      when MIT                  => "MIT",
+      when MPL_2_0              => "MPL 2.0",
+      when MS_PL                => "MS PL",
+      when MS_RL                => "MS RL",
+      when NCSA                 => "NCSA",
+      when OFL_1_1              => "OFL 1.1",
+      when OSL_3_0              => "OSL 3.0",
+      when PostgreSQL           => "PostgreSQL",
+      when Unlicense            => "Unlicense",
+      when WTFPL                => "WTFPL",
+      when Zlib                 => "zlib",
 
       --  Extra additions
-      when GMGPL_2_0            => +"GMGPL 2.0",
-      when GMGPL_3_0            => +"GMGPL 3.0",
-      when Public_Domain        => +"Public Domain",
-      when Unknown              => +"Unknown");
+      when GMGPL_2_0            => "GMGPL 2.0",
+      when GMGPL_3_0            => "GMGPL 3.0",
+      when Public_Domain        => "Public Domain",
+      when Custom               => "Custom",
+      when Unknown              => "Unknown");
 
    function From_String (Label : String) return Licenses;
    --  Return the Licenses value corresponding to Label (see License_Labels).
