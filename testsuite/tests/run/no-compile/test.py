bee96707002efc677befcb57ff31063e288da7ac
Fabien Chouteau <fabien.chouteau@gmail.com>
2019-12-05 11:40:00 +0100

    Testsuite: fix tests on Windows

--- testsuite/tests/run/no-compile/test.py
+++ testsuite/tests/run/no-compile/test.py
@@ -5,7 +5,7 @@ Check that running "alr run -s" fails when the project was not compiled.
 import os
 
 from drivers.alr import run_alr
-from drivers.asserts import assert_eq
+from drivers.asserts import assert_match
 
 
 # Create a test project
@@ -15,8 +15,8 @@ os.chdir('xxx')
 # Run it without compiling it first
 p = run_alr('run', '-s', complain_on_error=False)
 assert p.status != 0
-assert_eq('ERROR: Executable "xxx" not found\n'
-          'ERROR: alr run unsuccessful\n', 
-          p.out)
+assert_match('ERROR: Executable "xxx(\.exe)?" not found\n'
+             'ERROR: alr run unsuccessful\n', 
+             p.out)
 
 print('SUCCESS')
