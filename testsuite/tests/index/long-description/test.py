139fc4659104bfc517bd145d8256af647f98bfdf
Alejandro R. Mosteo <amosteo@unizar.es>
2019-12-17 14:20:44 +0100

    Test for valid long descriptions
    There was already a test name long-description, but was intended
    to test that the short description is not too long. This has been
    renamed as too-long-short-description.

--- testsuite/tests/index/long-description/test.py
+++ testsuite/tests/index/long-description/test.py
@@ -1,17 +1,17 @@
 """
-Test that long descriptions are rejected
+Test that long descriptions are accepted
 """
 
 from drivers.alr import run_alr
 from drivers.asserts import assert_match
 
+import re
 
-p = run_alr('show', 'hello_world',
-            complain_on_error=False, debug=False, quiet=True)
-assert_match('ERROR: Loading crate .*hello_world.toml:'
-             ' general: description: Description string is too long \(must be no more than [0-9]+\)\n'
-             'ERROR: Cannot read valid property from description\n'
-             'ERROR: alr show unsuccessful\n',
-    p.out)
+p = run_alr('show', 'hello_world')
+assert_match('.*Long_Description: This is the long crate description.\n'
+             '\n'
+             'This can be a multiline string, and TOML parsers are free to use'
+             ' the platform end-of-line character during loading.*',
+             p.out, flags=re.S)
 
 print('SUCCESS')
---
+++ testsuite/tests/index/too-long-short-description/test.py
@@ -0,0 +1,17 @@
+"""
+Test that too long descriptions are rejected
+"""
+
+from drivers.alr import run_alr
+from drivers.asserts import assert_match
+
+
+p = run_alr('show', 'hello_world',
+            complain_on_error=False, debug=False, quiet=True)
+assert_match('ERROR: Loading crate .*hello_world.toml:'
+             ' general: description: Description string is too long \(must be no more than [0-9]+\)\n'
+             'ERROR: Cannot read valid property from description\n'
+             'ERROR: alr show unsuccessful\n',
+    p.out)
+
+print('SUCCESS')
