13e01367959de48cbd59815c88aa7a26a26b6e3f
Alejandro R. Mosteo <amosteo@unizar.es>
2019-12-29 20:09:19 +0100

    Fix tests that failed on new extra stderr output

--- testsuite/tests/index/bad-name/test.py
+++ testsuite/tests/index/bad-name/test.py
@@ -7,7 +7,7 @@ from drivers.asserts import assert_match
 
 
 p = run_alr('index', '--add', 'xx', '--name', 'xx',
-            complain_on_error=False)
+            complain_on_error=False, debug=False)
 assert_match('.*Identifier too short.*', p.out)
 
 print('SUCCESS')
