8d94bdf9d8ae0a675fca73577d90aaba3b1608c4
Alejandro R. Mosteo <amosteo@unizar.es>
2020-06-07 14:43:03 +0200

    Test templates to help with creating new tests

--- testsuite/drivers/helpers.py
+++ testsuite/drivers/helpers.py
@@ -8,7 +8,7 @@ import os
 # Check a file contains a concrete line
 def check_line_in(filename, line):
     """
-    Assert that the `filename` tetx file contains at least one line that
+    Assert that the `filename` text file contains at least one line that
     contains `line`.
     """
     with open(filename, 'r') as f:
@@ -18,6 +18,11 @@ def check_line_in(filename, line):
         else:
             assert False, 'Could not find {} in {}'.format(
                 repr(line), filename)
+
+
+# Assert two values are equal or format the differences
+def compare(found, wanted):
+    assert found == wanted, 'Got:    {}\nWanted: {}'.format(found, wanted)
 
 
 # Return the entries (sorted) under a given folder, both folders and files
@@ -26,23 +31,3 @@ def contents(dir):
     return sorted([os.path.join(root, name).replace('\\', '/') for
                    root, dirs, files in os.walk(dir)
                    for name in dirs + files])
-
-
-# Assert two values are equal or format the differences
-def compare(found, wanted):
-    assert found == wanted, 'Got:    {}\nWanted: {}'.format(found, wanted)
-
-
-# Check line appears in file
-def check_line_in(filename, line):
-    """
-    Assert that the `filename` tetx file contains at least one line that
-    contains `line`.
-    """
-    with open(filename, 'r') as f:
-        for l in f:
-            if l.rstrip() == line:
-                break
-        else:
-            assert False, 'Could not find {} in {}'.format(
-                repr(line), filename)
---
+++ testsuite/skels/no-index/test.py
@@ -0,0 +1,12 @@
+"""
+A test that requires no indexes
+"""
+
+from drivers.alr import run_alr
+# from drivers.asserts import assert_eq, assert_match
+
+
+run_alr()
+
+
+print('SUCCESS')
