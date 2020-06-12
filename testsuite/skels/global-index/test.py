c81274aa8dbc96b21b5e07da4daf1d7d667d4501
Alejandro R Mosteo <alejandro@mosteo.com>
2020-06-12 15:22:57 +0200

    Test templates to help with creating new tests (#434)

---
+++ testsuite/skels/global-index/test.py
@@ -0,0 +1,12 @@
+"""
+A test that uses an index in testsuite/fixtures
+"""
+
+from drivers.alr import run_alr
+# from drivers.asserts import assert_eq, assert_match
+
+
+p = run_alr('clean')
+
+
+print('SUCCESS')
---
+++ testsuite/skels/local-index/test.py
@@ -0,0 +1,12 @@
+"""
+A test that uses its own local index
+"""
+
+from drivers.alr import run_alr
+# from drivers.asserts import assert_eq, assert_match
+
+
+run_alr()
+
+
+print('SUCCESS')
