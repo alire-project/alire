d463664549ecb3612b98ab4f6453daa5c38cfe5f
Alejandro R. Mosteo <amosteo@unizar.es>
2020-07-02 13:15:14 +0200

    Alire.Roots: move expression functions to body
    There's some bug manifesting when a lot of code is in the spec in which the
    linker complaints about missing references.

--- src/alire/alire-roots.adb
+++ src/alire/alire-roots.adb
@@ -2,6 +2,9 @@ with Ada.Directories;
 
 with Alire.Directories;
 with Alire.Environment;
+with Alire.Lockfiles;
+with Alire.OS_Lib;
+with Alire.Paths;
 with Alire.Root;
 with Alire.TOML_Index;
 
@@ -25,6 +28,7 @@ package body Alire.Roots is
    -----------------
 
    function Detect_Root (Path : Any_Path) return Root is
+      use Alire.OS_Lib;
       use GNAT.OS_Lib;
       Alire_Path : constant Any_Path :=
                      Path / Alire.Paths.Working_Folder_Inside_Root;
@@ -74,12 +78,42 @@ package body Alire.Roots is
       Context.Export;
    end Export_Build_Environment;
 
+   -----------------------
+   -- GPR_Project_Files --
+   -----------------------
+
+   function GPR_Project_Files (This         : Root;
+                               Exclude_Root : Boolean)
+                               return Utils.String_Set
+   is
+      Files : Utils.String_Set;
+   begin
+
+      --  Add files from every release in the solution
+
+      for Rel of This.Solution.Releases.Including (Release (This)) loop
+
+         if (not Exclude_Root or else Rel.Name /= Release (This).Name)
+           and then
+            Rel.Auto_GPR_With
+         then
+            for File of Rel.Project_Files
+              (This.Environment, With_Path => False)
+            loop
+               Files.Include (File);
+            end loop;
+         end if;
+      end loop;
+      return Files;
+   end GPR_Project_Files;
+
    -------------------
    -- Project_Paths --
    -------------------
 
    function Project_Paths (This : Root) return Utils.String_Set
    is
+      use Alire.OS_Lib;
       Paths : Utils.String_Set;
    begin
 
@@ -110,33 +144,85 @@ package body Alire.Roots is
       return Paths;
    end Project_Paths;
 
-   -----------------------
-   -- GPR_Project_Files --
-   -----------------------
+   --------------
+   -- Solution --
+   --------------
 
-   function GPR_Project_Files (This         : Root;
-                               Exclude_Root : Boolean)
-                               return Utils.String_Set
-   is
-      Files : Utils.String_Set;
+   function Solution (This : Root) return Solutions.Solution is
    begin
+      --  TODO: This probably is a good target for caching unless file
+      --  timestamp has changed.
+      return Lockfiles.Read (This.Lock_File);
+   end Solution;
 
-      --  Add files from every release in the solution
+   function Environment (This : Root) return Properties.Vector
+   is (This.Environment);
 
-      for Rel of This.Solution.Releases.Including (Release (This)) loop
+   function Is_Valid (This : Root) return Boolean is (This.Valid);
 
-         if (not Exclude_Root or else Rel.Name /= Release (This).Name)
-           and then
-            Rel.Auto_GPR_With
-         then
-            for File of Rel.Project_Files
-              (This.Environment, With_Path => False)
-            loop
-               Files.Include (File);
-            end loop;
-         end if;
-      end loop;
-      return Files;
-   end GPR_Project_Files;
+   function New_Invalid_Root return Root is
+     (Valid => False, Reason => +"");
+
+   function With_Reason (This : Root; Reason : String) return Root is
+     (Valid  => False,
+      Reason => +Reason);
+
+   function Invalid_Reason (This : Root) return String is
+     (+This.Reason);
+
+   function New_Root (Name : Crate_Name;
+                      Path : Absolute_Path;
+                      Env  : Properties.Vector) return Root is
+     (True,
+      Env,
+      +Path,
+      Containers.To_Release_H (Releases.New_Working_Release (Name)));
+
+   function New_Root (R    : Releases.Release;
+                      Path : Absolute_Path;
+                      Env  : Properties.Vector) return Root is
+     (True,
+      Env,
+      +Path,
+      Containers.To_Release_H (R));
+
+   function Path (This : Root) return Absolute_Path is (+This.Path);
+
+   function Release (This : Root) return Releases.Release is
+     (This.Release.Constant_Reference);
+
+   function Release (This  : Root;
+                     Crate : Crate_Name) return Releases.Release is
+     (if This.Release.Element.Name = Crate
+      then This.Release.Element
+      else This.Solution.State (Crate).Release);
+
+   use OS_Lib;
+
+   function Release_Base (This : Root; Crate : Crate_Name) return Any_Path is
+     (if This.Release.Element.Name = Crate then
+         +This.Path
+      elsif This.Solution.State (Crate).Is_Solved then
+          (+This.Path)
+      / Paths.Working_Folder_Inside_Root
+      / Paths.Dependency_Dir_Inside_Working_Folder
+      / Release (This, Crate).Unique_Folder
+      elsif This.Solution.State (Crate).Is_Linked then
+           This.Solution.State (Crate).Link.Path
+      else
+         raise Program_Error with "release must be either solved or linked");
+
+   function Lock_File (This : Root) return Absolute_Path is
+     (Lockfiles.File_Name
+        (This.Release.Constant_Reference.Name,
+         +This.Path));
+
+   function Crate_File (This : Root) return Absolute_Path is
+     (This.Working_Folder /
+        This.Release.Constant_Reference.Name_Str &
+        Paths.Crate_File_Extension_With_Dot);
+
+   function Working_Folder (This : Root) return Absolute_Path is
+     ((+This.Path) / "alire");
 
 end Alire.Roots;
