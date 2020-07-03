2a6c4f02c9f1b31b11a3038f44233921345dae10
Fabien Chouteau <chouteau@adacore.com>
2020-07-03 18:43:54 +0200

    Alire.{Root|Release}: remove trailing path separator in Roots.Project_Paths
    Doing it here means that case "base_dir/<empty_path>" is also handled.
    i.e. return "base_dir" instead of "base_dir/"

--- src/alire/alire-releases.adb
+++ src/alire/alire-releases.adb
@@ -428,10 +428,7 @@ package body Alire.Releases is
          for File of Files loop
             if Contains (File, "/") then
                Paths.Include
-                 (File (File'First .. Fixed.Index (File, "/", Backward) - 1));
-               --  To match the output of root crate paths and Ada.Directories
-               --  full path normalization, a path separator in the last
-               --  position is not included.
+                 (File (File'First .. Fixed.Index (File, "/", Backward)));
             else
 
                --  The project file is at the root of the release
