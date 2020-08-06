c20acf8308666e5acfd20b971c7e2d0dc22eb8b0
Alejandro R. Mosteo <amosteo@unizar.es>
2020-08-06 18:16:28 +0200

    Testsuite adjustments for extra error output
    Since we now take advantage of the manifest provenance (index/local), some
    error messages require tweaking.

--- testsuite/tests/index/bad-license/test.py
+++ testsuite/tests/index/bad-license/test.py
@@ -8,7 +8,7 @@ from drivers.asserts import assert_match
 
 p = run_alr('show', 'hello_world',
             complain_on_error=False, debug=False, quiet=True)
-assert_match('ERROR: Loading .*hello_world-0.1.0.toml: '
+assert_match('ERROR:.*Loading .*hello_world-0.1.0.toml: '
              'licenses: unknown license: \'Invalid license ID\'\n',
              p.out)
 
--- testsuite/tests/index/too-long-short-description/test.py
+++ testsuite/tests/index/too-long-short-description/test.py
@@ -8,9 +8,11 @@ from drivers.asserts import assert_match
 
 p = run_alr('show', 'hello_world',
             complain_on_error=False, debug=False, quiet=True)
-assert_match('ERROR: Loading .*hello_world-0.1.0.toml:'
+assert_match('ERROR:.*Loading .*hello_world-0.1.0.toml:'
              ' description: Description string is too long'
-             ' \(must be no more than [0-9]+\)\n',
+             ' \(must be no more than [0-9]+\)\n'
+             'ERROR: alr encountered an unexpected error,'
+             ' re-run with -d for details.',
              p.out)
 
 print('SUCCESS')
