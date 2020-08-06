41e29421001032ff81e67d4a05fb2f7f196be2be
Alejandro R. Mosteo <amosteo@unizar.es>
2020-08-06 19:40:19 +0200

    Fix testsuite output matches on new line breaks

--- testsuite/tests/index/external-msys2/test.py
+++ testsuite/tests/index/external-msys2/test.py
@@ -8,15 +8,15 @@ import platform
 
 if platform.system() == 'Windows':
     # Should silently retrieve everything
-    p = run_alr('--non-interactive', '-v', 'get', 'main', quiet=False, debug=True)
-    
+    p = run_alr('--non-interactive', '-v', 'get', 'main',
+                quiet=False, debug=True)
+
     checks = 0
     for line in p.out.splitlines():
         if line.startswith("cdialog (ComeOn Dialog!) version "):
             print("dialog output matched")
             checks += 1
-    
+
     assert checks == 1, 'Only %d match in the output : "%s"' % (checks, p.out)
 
 print('SUCCESS')
-    
