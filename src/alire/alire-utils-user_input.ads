c0516ee47b06ff3923a1859120a75ebb4c0a9a7d
Alejandro R. Mosteo <amosteo@unizar.es>
2020-09-27 18:17:27 +0200

    Break circular elaboration issue
    Since Config uses Utils.User_Input indirectly via Deplyers.System, a
    possibility is to move new User_Input functions that need Config into a new
    package.

--- src/alire/alire-utils-user_input.ads
+++ src/alire/alire-utils-user_input.ads
@@ -63,16 +63,6 @@ package Alire.Utils.User_Input is
    --
    --  The Default value has to be a valid input.
 
-   function Config_Or_Query_String (Config_Key : String;
-                                    Question   : String;
-                                    Default    : String;
-                                    Validation : String_Validation_Access)
-                                    return String
-     with Pre => Validation = null or else Validation (Default);
-   --  Same as Query_String but first looks for a configuration value before
-   --  querying the user. If the answer is different from Default, it is saved
-   --  in the global configuration.
-
    procedure Continue_Or_Abort;
    --  If interactive, ask the user to press Enter or Ctrl-C to stop.
    --  Output a log trace otherwise and continue.
@@ -85,21 +75,5 @@ package Alire.Utils.User_Input is
    --  Present a summary of changes and ask the user for confirmation. Returns
    --  True when the user answers positively. Defaults to Yes when the new
    --  solution is complete, or when Alire.Force.
-
-   ---------------------
-   -- Query or config --
-   ---------------------
-   --  The following fuction will get their value from:
-   --   - The config if defined
-   --   - The user if in interactive mode
-   --   - A default value otherwise
-
-   function User_Name return String;
-
-   function User_GitHub_Login return String
-     with Post => (Is_Valid_GitHub_Username (User_GitHub_Login'Result));
-
-   function User_Email return String
-     with Post => Could_Be_An_Email (User_Email'Result, With_Name => False);
 
 end Alire.Utils.User_Input;
