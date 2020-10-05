dcfe3031d55c8abfd2b47fd406bea7fdb4130a97
Fabien Chouteau <Fabien-Chouteau@users.noreply.github.com>
2020-10-05 10:22:39 +0200

    Fix style

--- src/alr/os_windows/alr-platforms-windows.adb
+++ src/alr/os_windows/alr-platforms-windows.adb
@@ -19,7 +19,7 @@ package body Alr.Platforms.Windows is
 
    Msys2_Installer     : constant String := "msys2-x86_64-20200903.exe";
    Msys2_Installer_URL : constant String :=
-     "https://github.com/msys2/msys2-installer/releases/download/2020-09-03/" 
+     "https://github.com/msys2/msys2-installer/releases/download/2020-09-03/"
      & Msys2_Installer;
 
    Msys2_Installer_Script     : constant String := "auto-install.js";
