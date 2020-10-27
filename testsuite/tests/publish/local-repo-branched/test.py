8d4fa407ef83f9bc52dee31454ba4bdd6c613345
Alejandro R Mosteo <alejandro@mosteo.com>
2020-10-27 14:58:58 +0100

    Git: Use remote branch when checking remote status (#596)
    Otherwise it seems git defaults to the main branch, which manifested as
    spurious "ahead" status, or ambiguous references in local remotes.

---
+++ testsuite/tests/publish/local-repo-branched/test.py
@@ -0,0 +1,34 @@
+"""
+Test that publishing from a branch other than the default one works
+"""
+
+from drivers.alr import init_local_crate, run_alr
+from drivers.asserts import assert_match
+from drivers.helpers import init_git_repo
+from glob import glob
+from shutil import copyfile
+from subprocess import run
+
+import os
+
+# Prepare our "remote" repo
+init_local_crate("xxx", enter=False)
+head_commit = init_git_repo("xxx")
+
+# Clone to a "local" repo and set minimal config
+assert run(["git", "clone", "xxx", "xxx_local"]).returncode == 0
+os.chdir("xxx_local")
+assert run(["git", "config", "user.email", "alr@testing.com"]).returncode == 0
+assert run(["git", "config", "user.name", "Alire Testsuite"]).returncode == 0
+
+# Check out a new branch, add some file and push it to the "remote"
+assert run(["git", "checkout", "-b", "devel"]).returncode == 0
+copyfile("alire.toml", "noise.bin")
+assert run(["git", "add", "."]).returncode == 0
+assert run(["git", "commit", "-m", "commit-msg"]).returncode == 0
+assert run(["git", "push", "-u", "origin", "devel"]).returncode == 0
+
+# Check that the publishing assistant completes without complaining
+run_alr("--force", "publish")
+
+print('SUCCESS')
