36f84ac7fcb4b1bb2027183375b1f6a79e725b28
Fabien Chouteau <Fabien-Chouteau@users.noreply.github.com>
2021-05-19 20:48:44 +0200

    Check_Absolute_Path on Windows should also accept lowercase drive letters
    #739

--- src/alire/os_windows/alire-check_absolute_path.adb
+++ src/alire/os_windows/alire-check_absolute_path.adb
@@ -2,7 +2,7 @@ separate (Alire)
 function Check_Absolute_Path (Path : Any_Path) return Boolean is
 begin
    return (Path'Length >= 3
-           and then Path (Path'First) in 'A' .. 'Z'
+           and then Path (Path'First) in 'A' .. 'Z' | 'a' .. 'z'
            and then Path (Path'First + 1) = ':'
            and then Path (Path'First + 2) = GNAT.OS_Lib.Directory_Separator);
 end Check_Absolute_Path;
