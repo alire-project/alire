908bf12bc77414aef13555bea8ef8accf368ff79
Alejandro R. Mosteo <amosteo@unizar.es>
2021-06-22 10:08:31 +0200

    Test for self-pinning and circularities

--- testsuite/tests/pin/circularity/test.py
+++ testsuite/tests/pin/circularity/test.py
@@ -7,6 +7,7 @@ from drivers.asserts import assert_eq, assert_match
 from drivers.helpers import on_windows
 
 import os
+import shutil
 
 # Obvious self-pinning detection
 init_local_crate()
@@ -15,6 +16,29 @@ p = run_alr("pin", complain_on_error=False)
 assert_match(".*"
              "ERROR: Pin circularity detected when adding pin xxx --> xxx:\n"
              "ERROR:    Last manifest in the cycle is .*\n",
+             p.out)
+
+
+# A real cycle detection
+os.chdir("..")    # back to top-level
+shutil.rmtree("xxx")  # and restart from scratch
+
+# Prepare a cycle
+init_local_crate("yyy", enter=False)
+init_local_crate("zzz")
+alr_pin("yyy", path="../yyy", update=False)
+os.chdir("..")
+os.chdir("yyy")
+alr_pin("zzz", path="../zzz", update=False)
+os.chdir("..")
+init_local_crate("xxx")
+alr_pin("yyy", path="../yyy", update=False)
+
+# At this point, xxx --> yyy --> zzz --> yyy
+p = run_alr("pin", complain_on_error=False)
+assert_match(".*"
+             "ERROR: Pin circularity detected when adding pin zzz --> yyy:\n"
+             "ERROR:    Last manifest in the cycle is .*/zzz/alire.toml\n",
              p.out)
 
 print('SUCCESS')
