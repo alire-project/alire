37ba1465965c5535bbcc0910fe493edd0fb958e2
Alejandro R Mosteo <alejandro@mosteo.com>
2021-08-18 13:11:06 +0200

    Reinstate "forbids" property (#781)
    * Load "forbids" property, and test
    
    * Update catalog spec document
    
    * doc/user-changes.md: documented "forbids" property

---
+++ testsuite/tests/solver/forbids-replace/test.py
@@ -0,0 +1,35 @@
+"""
+Test the usual use case in which a drop-in replacement crate forbids equivalent
+crates in the solution. This works by both providing and forbidding the same
+crate, which incidentally is the same modus operandi of apt-get "conflicts":
+https://www.debian.org/doc/manuals/debian-reference/ch02.en.html#_package_dependencies
+"""
+
+import subprocess
+import os
+
+from drivers.alr import run_alr, init_local_crate, alr_with
+from drivers.asserts import assert_eq, assert_match, match_solution
+from re import escape as e
+
+# This test relies on two crates in the toolchain_index:
+# crate_subst both provides and forbids crate_real
+# crate_real is a regular crate only provided by itself and crate_subst
+
+# The following has only one possible solution, which is for crate_subst
+# providing both dependencies.
+init_local_crate("test")
+alr_with("crate_real")
+
+# Check that this is initially solved with the regular crate. This is currently
+# guaranteed by the solver attempting crates in alphabetical order. We will
+# need eventually a way to disable equivalences (via pins, or solver config).
+match_solution("crate_real=1.0.0 (origin: filesystem)", escape=True)
+
+# Let's add the drop-in equivalent crate that provides+forbids crate_lone
+alr_with("crate_subst")
+match_solution("crate_real=1.0.0 (crate_subst) (origin: filesystem)",
+               escape=True)  # This is the substituted release
+match_solution("crate_subst=1.0.0 (origin: filesystem)", escape=True)
+
+print('SUCCESS')
---
+++ testsuite/tests/solver/forbids/test.py
@@ -0,0 +1,33 @@
+"""
+Test that the forbids field works for regular and provided crates
+"""
+
+import subprocess
+import os
+
+from drivers.alr import run_alr, init_local_crate, alr_with
+from drivers.asserts import assert_eq, assert_match, match_solution
+from re import escape as e
+
+# This test relies on three crates in the toolchain_index:
+# crate_conflict=1.2.3 conflicts with crate_lone* and crate_virtual*
+# crate_lone is a regular crate
+# crate_virtual has no releases, but is provided by crate_conflict_1
+# Crate conflict cannot appear with any of the others in a solution, because of
+# its [forbids] table.
+
+init_local_crate("conflict_lone")
+alr_with("crate_conflict")
+alr_with("crate_lone")
+match_solution("crate_(conflict|lone)=.* \(origin:.*\)")   # has origin: solved
+match_solution("crate_(conflict|lone)\* \(direct,missed\)")
+# Because of load/solving details, we do not know which of the two crates is
+# going to be missed/accepted in the solution, so we check there is one of each
+
+init_local_crate("conflict_virtual")
+alr_with("crate_conflict")
+alr_with("crate_virtual")
+match_solution("crate_(conflict|virtual)=.* \(origin:.*\)")
+match_solution("crate_(conflict|virtual)\* \(direct,missed\)")
+
+print('SUCCESS')
