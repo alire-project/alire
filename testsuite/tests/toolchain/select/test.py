291de26f3476398ab426c9fb42d3fb46783532b5
Alejandro R. Mosteo <amosteo@unizar.es>
2021-08-24 11:02:40 +0200

    Enhance toolchain selection test
    Now we test the non-interactive selection, and the local selection

--- testsuite/tests/toolchain/select/test.py
+++ testsuite/tests/toolchain/select/test.py
@@ -2,10 +2,11 @@
 Check toolchain selection assistant
 """
 
-import subprocess
+import os
 import re
+import subprocess
 
-from drivers.alr import run_alr
+from drivers.alr import run_alr, init_local_crate
 from drivers.asserts import assert_eq, assert_match
 
 # Activate the default compiler
@@ -14,6 +15,24 @@ p = run_alr("toolchain", "--select")
 # Check that the newest native compiler is the Default now (vs Available)
 p = run_alr("toolchain")
 assert_match(".*gnat_native.*" + re.escape("2.0.0") + ".*Default.*",
+             p.out)
+
+# Select an older compiler as default
+run_alr("toolchain", "--select", "gnat_native=1")
+p = run_alr("toolchain")
+assert_match(".*gnat_native.*" + re.escape("1.0.0") + ".*Default.*",
+             p.out)
+
+# Test local selection by configuring locally inside a crate
+init_local_crate()
+run_alr("toolchain", "--select", "gnat_native=2", "--local")
+p = run_alr("toolchain")
+assert_match(".*gnat_native.*" + re.escape("2.0.0") + ".*Default.*",
+             p.out)
+# And check that outside the global selection is still in effect
+os.chdir("..")
+p = run_alr("toolchain")
+assert_match(".*gnat_native.*" + re.escape("1.0.0") + ".*Default.*",
              p.out)
 
 # I've (mosteo) been unable to connect stdin with an alr launched via #
