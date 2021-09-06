67d2688c68016d12ec758f8106f74fba3be6e05c
Fabien Chouteau <chouteau@adacore.com>
2021-09-06 11:14:22 +0200

    tests/toolchain/custom_install_dir: fix paths for Windows and second dir

--- testsuite/tests/toolchain/custom_install_dir/test.py
+++ testsuite/tests/toolchain/custom_install_dir/test.py
@@ -9,16 +9,16 @@ from drivers.alr import run_alr, init_local_crate, alr_with
 from drivers.asserts import assert_eq, assert_match, match_solution
 from drivers.helpers import contents
 
-install_dir = os.path.join (os.getcwd(), "custom_install")
-install_dir_2 = os.path.join (os.getcwd(), "custom_install_2")
+install_dir = os.path.join (os.getcwd(), "custom_install").replace("\\", "/")
+install_dir_2 = os.path.join (os.getcwd(), "custom_install").replace("\\", "/")
 
 
 unk_re = "[0-9]+\.[0-9]+\.[0-9]+_[0-9a-f]{8}"  # Unknown version + Unknown hash
 def config_path_re(crate):
     return re.escape(f"{install_dir}/{crate}_") + unk_re
 
-def check_content(crate):
-    paths = contents(install_dir, crate)
+def check_content(crate, dir):
+    paths = contents(dir, crate)
     try:
         assert len(paths) >= 1 and \
             re.search(config_path_re(crate), paths[0]), \
@@ -31,14 +31,14 @@ def check_content(crate):
 # First we test installation of one component
 run_alr("toolchain", "--install", "gnat_native", "--install-dir", install_dir)
 
-check_content("gnat_native")
+check_content("gnat_native", install_dir)
 
 
 # We now test installation of two components
 run_alr("toolchain", "--install", "gnat_native", "gnat_cross_1",
-        "--install-dir", install_dir)
+        "--install-dir", install_dir_2)
 
-check_content("gnat_native")
-check_content("gnat_cross_1")
+check_content("gnat_native", install_dir_2)
+check_content("gnat_cross_1", install_dir_2)
 
 print('SUCCESS')
