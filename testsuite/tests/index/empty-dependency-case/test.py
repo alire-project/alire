22f885975f3946048636a2ea227b817e3bb72165
Alejandro R. Mosteo <amosteo@unizar.es>
2022-01-03 14:37:53 +0100

    Test for bug

---
+++ testsuite/tests/index/empty-dependency-case/test.py
@@ -0,0 +1,21 @@
+"""
+Test that an empty nested table in dependencies does not cause an error.
+Bugfix #
+"""
+
+from drivers.alr import run_alr, init_local_crate, alr_manifest
+from drivers.asserts import assert_match
+
+init_local_crate()
+
+# Create the problematic table
+with open(alr_manifest(), "at") as manifest:
+    manifest.write("[[depends-on]]\n")
+    manifest.write("[depends-on.'case(os)'.linux."
+                   "'case(distribution)'.ubuntu]\n")
+
+# The following command failed pre-bugfix, all is OK if it does not complain
+p = run_alr("update")
+
+
+print('SUCCESS')
