b9a088ede2211792b09a183ed929bc88a3e0c5df
Alejandro R. Mosteo <amosteo@unizar.es>
2022-02-03 21:26:01 +0100

    Alire.Root: remove no longer needed Env copy

--- src/alire/alire-root.adb
+++ src/alire/alire-root.adb
@@ -1,5 +1,4 @@
 with Alire.Directories;
-with Alire.Properties.Platform;
 
 package body Alire.Root is
 
@@ -16,44 +15,5 @@ package body Alire.Root is
 
    function Current return Roots.Root
    is (Roots.Optional.Detect_Root (Directories.Detect_Root_Path).Value);
-
-   -------------------------
-   -- Platform_Properties --
-   -------------------------
-
-   Environment : Properties.Vector;
-   OS          : Platforms.Operating_Systems := Platforms.OS_Unknown;
-
-   -------------------------
-   -- Platform_Properties --
-   -------------------------
-
-   function Platform_Properties return Properties.Vector
-   is (Environment);
-
-   -----------------
-   -- Platform_OS --
-   -----------------
-
-   function Platform_OS return Platforms.Operating_Systems
-   is (OS);
-
-   -----------------------------
-   -- Set_Platform_Properties --
-   -----------------------------
-
-   procedure Set_Platform_Properties (Env : Properties.Vector) is
-   begin
-      Environment := Env;
-
-      --  Extract the current OS for easier use
-
-      for Prop of Env loop
-         if Prop in Properties.Platform.Operating_Systems.Property'Class then
-            OS := Properties.Platform.Operating_Systems.Property'Class (Prop)
-              .Element;
-         end if;
-      end loop;
-   end Set_Platform_Properties;
 
 end Alire.Root;
--- src/alr/alr-main.adb
+++ src/alr/alr-main.adb
@@ -1,16 +1,11 @@
-with Alire;
 with Alire_Early_Elaboration; pragma Elaborate_All (Alire_Early_Elaboration);
-with Alire.Platforms.Current;
 with Alire.Platforms.Init;    pragma Elaborate_All (Alire.Platforms.Init);
-with Alire.Root;
 
 with Alr.Commands;
 with Alr.Last_Chance_Handler;
 
 procedure Alr.Main is
 begin
-   Alire.Root.Set_Platform_Properties (Alire.Platforms.Current.Properties);
-
    Trace.Debug ("alr platform configured");
 
    Commands.Execute;
