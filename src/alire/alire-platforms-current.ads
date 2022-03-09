5d940bc0a7733759a36b2af492bd785f40904902
Alejandro R Mosteo <amosteo@unizar.es>
2022-03-09 16:04:03 +0100

    Leftover comments from platform refactor (#950)

--- src/alire/alire-platforms-current.ads
+++ src/alire/alire-platforms-current.ads
@@ -10,9 +10,7 @@ private with System;
 
 package Alire.Platforms.Current is
 
-   --  Alr.Platforms will be progressively migrated in here as needed.
-
-   --  These specs must be fulfilled by bodies in each different OS we support.
+   --  This spec must be fulfilled by bodies for each different OS we support
 
    -------------------
    --  Low level stuff
@@ -21,17 +19,18 @@ package Alire.Platforms.Current is
    --  Folder where alire will store its global configuration, indexes, and
    --  any other global data. Deleting it is akin to running alr afresh for
    --  the first time.
-
-   --  On Linux it is ${XDG_CONFIG_HOME:-$HOME/.config}/alire
+   --  On Linux/macOS it is ${XDG_CONFIG_HOME:-$HOME/.config}/alire
+   --  On Windows it is $Homedrive:$Homepath\.config\alire
 
    function Cache_Folder return String;
    --  Folder for dependencies, global toolchains, and any other info that is
    --  not critical to lose. Can be deleted freely, it's repopulated on-demand.
-
-   --  On Linux it is ${XDG_CACHE_HOME:-$HOME/.cache}/alire
+   --  On Linux/macOS it is ${XDG_CACHE_HOME:-$HOME/.cache}/alire
+   --  On Windows it is $Homedrive:$Homepath\.cache\alire
 
    function Distribution_Root return Absolute_Path;
-   --  Root directory of the distribution
+   --  Root directory of the distribution; on unixes it is "/", on Windows it
+   --  is the root of our msys2 installation.
 
    procedure Load_Environment (Ctx : in out Alire.Environment.Context);
    --  Set environment variables from the platform. Used by Windows to
