a4937b864d7afd0eb61f9ee4ae0b3ce1c14c4fa7
Piotr Orzechowski <piotr@orzechowski.tech>
2022-12-12 02:34:10 +0100

    Add more test cases for bad index name

--- testsuite/tests/index/bad-name/test.py
+++ testsuite/tests/index/bad-name/test.py
@@ -1,13 +1,27 @@
-"""
+'''
 Check that specifying a malformed index name is properly reported
-"""
+'''
 
 from drivers.alr import run_alr
 from drivers.asserts import assert_match
 
+def assert_that(name, fails_with):
+    p = run_alr('index', '--add', name, '--name', name, complain_on_error=False, debug=False)
+    assert_match(fails_with, p.out)
 
-p = run_alr('index', '--add', 'xx', '--name', 'xx',
-            complain_on_error=False, debug=False)
-assert_match('.*Identifier too short.*', p.out)
+# < min length
+assert_that(name='aa', fails_with='.*Identifier too short.*')
+
+# > max length
+assert_that(name='a' * 65, fails_with='.*Identifier too long.*')
+
+# Leading underscore
+assert_that(name='_aaa', fails_with='.*Identifiers must not begin with an underscore.*')
+
+# Leading dot
+assert_that(name='.aaa', fails_with='.*Identifiers must not begin with a dot.*')
+
+# Non lowercase ASCII alnum
+assert_that(name='aaą', fails_with='.*Identifiers must be lowercase ASCII alphanumerical.*')
 
 print('SUCCESS')
