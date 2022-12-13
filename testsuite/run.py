a5aa0e399b5d790c7cdc37e21b7c7f4d84e68b4f
Piotr Orzechowski <piotr@orzechowski.tech>
2022-12-13 19:53:36 +0100

    Use `alr` from project bin dir in testsuite (#1262)
    * Mention Python 3 testsuite run script's comment
    
    * Prepend alr project dir to PATH before running tests
    
    * Fail user input validation test explicitly
    
    Use Polish letter 'ł' to make user input validation test
    explicitly mention encoding problems when failing.

--- testsuite/run.py
+++ testsuite/run.py
@@ -3,7 +3,7 @@
 """
 e3.testsuite-based testsuite for Alire/ALR.
 
-Just execute this script to run the testsuite. It requires a Python2
+Just execute this script to run the testsuite. It requires a Python 3
 interpreter with the e3-core and e3-testsuite packages (from PyPI) installed.
 """
 
@@ -30,6 +30,16 @@ class Testsuite(e3.testsuite.Testsuite):
         # Some tests rely on an initially empty GPR_PROJECT_PATH variable
         os.environ.pop('GPR_PROJECT_PATH', None)
 
+        # Let's run `alr` from project's dir
+        alr_path = self._get_alr_path()
+        self._prepend_alr_to_path_env_var(alr_path)
+
+    def _get_alr_path(self):
+        project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        return os.path.join(project_root, 'bin')
+
+    def _prepend_alr_to_path_env_var(self, alr_path):
+        os.environ['PATH'] = alr_path + os.pathsep + os.environ['PATH']
 
 if __name__ == '__main__':
     suite = Testsuite()
