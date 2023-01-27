c60524ef58d641d1c05ee0b1e5637569b4d88161
Alejandro R. Mosteo <amosteo@unizar.es>
2023-01-27 19:27:41 +0100

    Remove any implicit detection of the host OS
    This should avoid misbuilds that mix Linux/macOS.
    
    A bash function is now provided for detection of the OS in our scripts.

---
+++ dev/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Import reusable bits
+pushd $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) > /dev/null
+    . functions.sh
+popd > /dev/null
+
+export ALIRE_OS=$(get_OS)
+
+echo Building with ALIRE_OS=$ALIRE_OS...
+gprbuild -j0 -r -p -P `dirname $0`/../alr_env.gpr "$@"
