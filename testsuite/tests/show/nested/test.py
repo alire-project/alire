9170ca1938e1be2ab7ce22b0d04f2bd2b18402b8
Alejandro R. Mosteo <amosteo@unizar.es>
2023-02-22 17:29:31 +0100

    Test for `alr show --nested`

---
+++ testsuite/tests/show/nested/test.py
@@ -0,0 +1,31 @@
+"""
+Test working of `alr show --nested`
+"""
+
+from drivers.alr import run_alr, init_local_crate
+from drivers.asserts import assert_eq, assert_match
+
+import os
+
+#  We create a crate below us; this should be detected
+
+init_local_crate(enter=False)
+assert_match(".*Found 1 nested crate in .*:\n"
+             r"   xxx/xxx=0.1.0-dev: \(no description\)\n",
+             run_alr("show", "--nested", quiet=False).out)
+
+# After entering the crate, it is no longer nested and shouldn't be detected
+
+os.chdir("xxx")
+assert_match("\s*",
+             run_alr("show", "--nested", quiet=False).out)
+
+# If we initialize another crate without entering it, it should again be
+# detected
+
+init_local_crate(name="yyy", enter=False)
+assert_match(".*Found 1 nested crate in .*:\n"
+             r"   yyy/yyy=0.1.0-dev: \(no description\)\n",
+             run_alr("show", "--nested", quiet=False).out)
+
+print('SUCCESS')
