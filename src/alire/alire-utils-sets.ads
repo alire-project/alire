df60449d402c87326c631df4bb73cdbe4a57546b
Jesper Quorning <jesper.quorning@gmail.com>
2023-03-14 20:27:42 +0100

    Indefinite_Ordered

--- src/alire/alire-utils-sets.ads
+++ src/alire/alire-utils-sets.ads
@@ -11,28 +11,21 @@ is
             "="          => <>,
             "<"          => <>);
 
-   package Ordered
+   package Indefinite_Ordered
    is
       generic
          type Item_Type (<>) is private;
+  
+         Set : Sets.Set;
 
          with function "=" (
-            Set     : Sets.Set;
             Element : Element_Type;
             Item    : Item_Type)
             return Boolean;
    
-      package Indefinite
-      is
-         function Contains
-            (
-            Item : Item_Type
-            )
-         return Boolean;
+      function Contains (Item : Item_Type) return Boolean;
 
-      end Indefinite;
-
-   end Ordered;
+   end Indefinite_Ordered;
 
 end Alire.Utils.Sets;
 
