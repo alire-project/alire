c73a98acbcc847b144a195bfae8ed30bac4d6a6d
Jesper Quorning <jesper.quorning@gmail.com>
2023-03-14 14:58:10 +0100

    Restructure alire-utils-sets.ads

--- src/alire/alire-utils-sets.ads
+++ src/alire/alire-utils-sets.ads
@@ -4,26 +4,34 @@ package Alire.Utils.Sets
 is
    generic
       type Element_type (<>) is private;
-      type Item_Type (<>)    is private;
-
+ 
       with package Sets is new
          Ada.Containers.Indefinite_Ordered_Sets (
             Element_Type => Element_Type,
             "="          => <>,
             "<"          => <>);
-
-      with function "=" (
-         Item    : Item_Type;
-         Element : Element_Type)
-         return Boolean;
-   
-   package Indefinite_Ordered
+   package Ordered
    is
-      function Contains
-        (Container : Sets.Set;
-         Item      : Item_Type)
+      generic
+         type Item_Type (<>)    is private;
+
+         with function "=" (
+            Item    : Item_Type;
+            Element : Element_Type)
+            return Boolean;
+   
+      package Indefinite
+      is
+         function Contains
+            (
+            Container : Sets.Set;
+            Item      : Item_Type
+            )
          return Boolean;
 
-   end Indefinite_Ordered;
+      end Indefinite;
+
+   end Ordered;
 
 end Alire.Utils.Sets;
+
