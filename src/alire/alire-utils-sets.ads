700c0251850c1fb0246eb9af271466efc71154e4
Jesper Quorning <jesper.quorning@gmail.com>
2023-03-14 14:59:59 +0100

    Update alire-utils-sets.ads, minor

--- src/alire/alire-utils-sets.ads
+++ src/alire/alire-utils-sets.ads
@@ -3,17 +3,18 @@ with Ada.Containers.Indefinite_Ordered_Sets;
 package Alire.Utils.Sets
 is
    generic
-      type Element_type (<>) is private;
+      type Element_Type (<>) is private;
  
       with package Sets is new
          Ada.Containers.Indefinite_Ordered_Sets (
             Element_Type => Element_Type,
             "="          => <>,
             "<"          => <>);
+
    package Ordered
    is
       generic
-         type Item_Type (<>)    is private;
+         type Item_Type (<>) is private;
 
          with function "=" (
             Item    : Item_Type;
