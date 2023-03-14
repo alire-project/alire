0aaf72eedef0777ea2ad6137af727521c419be12
Jesper Quorning <jesper.quorning@gmail.com>
2023-03-14 13:38:57 +0100

    actual for "Item_Type" must be a definite subtype

--- src/alire/alire-utils-sets.ads
+++ src/alire/alire-utils-sets.ads
@@ -4,11 +4,11 @@ package Alire.Utils.Sets
 is
    generic
 --      type Element_type is private;
-      type Item_Type    is private;
+      type Item_Type (<>) is private;
 
       with package Sets is new
          Ada.Containers.Indefinite_Ordered_Sets (
-            Element_Type => <>,  --  Element_Type,
+            Element_Type => <>,
             "="          => <>,
             "<"          => <>);
 
