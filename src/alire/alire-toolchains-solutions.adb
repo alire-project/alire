8209b0e77d17fc9c6d46a9eb0da40ed791394af0
Alejandro R. Mosteo <amosteo@unizar.es>
2023-03-21 12:08:11 +0100

    Removed cache migration

--- src/alire/alire-toolchains-solutions.adb
+++ src/alire/alire-toolchains-solutions.adb
@@ -1,108 +1,10 @@
 with AAA.Strings;
 
-with Alire.Config.Edit;
-with Alire.Directories;
 with Alire.Index;
-with Alire.Paths;
-with Alire.Platforms.Folders;
 with Alire.Root;
 with Alire.Shared;
 
-with CLIC.User_Input;
-
 package body Alire.Toolchains.Solutions is
-
-   -------------------
-   -- Migrate_Cache --
-   -------------------
-
-   procedure Migrate_Cache is
-      use Directories.Operators;
-      Config_Flag : constant String := "alire.cache_migrated";
-      Old_Dir     : constant Absolute_Path
-        := Platforms.Folders.Config
-           / Paths.Cache_Folder_Inside_Working_Folder;
-   begin
-      if not Config.Edit.Is_At_Default_Dir then
-         return; -- Not using default configuration
-      end if;
-
-      if Config.DB.Get (Config_Flag, False) then
-         return; -- Already migrated
-      end if;
-
-      if not Directories.Is_Directory (Old_Dir / "dependencies") then
-         Config.Edit.Set_Globally (Config_Flag, "true");
-         return; -- Nothing to migrate
-      end if;
-
-      Put_Info
-        ("The storage location for compilers has changed to better adhere to "
-         & "best practices.");
-      Trace.Always ("  Old location: " & TTY.URL (Old_Dir));
-      Trace.Always ("  New Location: " & TTY.URL (Platforms.Folders.Cache));
-
-      Trace.Always ("");
-      Put_Info ("Alire will continue operating normally in any case, "
-                & "but you can choose what to do in this one-time operation to"
-                & " avoid large redownloads.");
-      Put_Info ("Unless you are using the old location explicitly somehow, "
-                & "it can be safely deleted.");
-      Trace.Always ("");
-
-      declare
-         type Choices is (Zero, Copy, Move, Pass);
-         Choice : constant Choices := Choices'Val (
-           CLIC.User_Input.Query_Multi
-             (Question => "Please choose your migration preference:",
-              Choices  => AAA.Strings.Empty_Vector
-              .Append ("Copy toolchains to new location")
-              .Append ("Move toolchains to new location")
-              .Append ("Do nothing (compilers will be redownloaded on demand)")
-             ));
-      begin
-         case Choice is
-            when Zero => raise Program_Error with "Query_Multi violated Post";
-            when Copy =>
-               Put_Info ("Copying...");
-               Directories.Create_Tree (Platforms.Folders.Cache);
-               Directories.Merge_Contents (Src => Old_Dir,
-                                           Dst => Platforms.Folders.Cache,
-                                           Skip_Top_Level_Files  => False,
-                                           Fail_On_Existing_File => False,
-                                           Remove_From_Source    => False);
-               Put_Success ("Contents copied, the old location can be deleted"
-                            & " at your convenience.");
-            when Move =>
-               Put_Info ("Moving...");
-               Directories.Create_Tree (Platforms.Folders.Cache);
-               Directories.Merge_Contents (Src => Old_Dir,
-                                           Dst => Platforms.Folders.Cache,
-                                           Skip_Top_Level_Files  => False,
-                                           Fail_On_Existing_File => False,
-                                           Remove_From_Source    => True);
-               Directories.Force_Delete (Old_Dir);
-               Put_Success ("Contents moved successfully.");
-            when Pass =>
-               Put_Info ("Did nothing, compilers will be redownloaded on "
-                         & " demand. The old location can be deleted at your"
-                         & " convenience.");
-         end case;
-      end;
-
-      Config.Edit.Set_Globally (Config_Flag, "true");
-   exception
-      when E : others =>
-         --  Since this involves HD use, a number of things can go wrong...
-         --  Don't reattempt, compilers will be redownloaded and the old
-         --  location will have to be deleted manually.
-         Log_Exception (E, Warning);
-         Config.Edit.Set_Globally (Config_Flag, "true");
-         Put_Warning ("The operation failed and will not be reattempted. "
-                      & " Compilers will be downloaded again when needed. "
-                      & " You can delete the old location manually to recover "
-                      & "disk space.");
-   end Migrate_Cache;
 
    -------------------
    -- Add_Toolchain --
@@ -132,11 +34,6 @@ package body Alire.Toolchains.Solutions is
 
       Result : Alire.Solutions.Solution := Solution;
    begin
-
-      --  For the users' sake, offer to relocate a toolchain in the former
-      --  cache location to the new location as gnat downloads are quite heavy.
-
-      Migrate_Cache;
 
       --  For every tool in the toolchain that does not appear in the solution,
       --  we will insert the user-configured tool, if any.
