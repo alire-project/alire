cccf9e33297ae3413a9831eeb9b5fcd2dddeadff
Alejandro R. Mosteo <amosteo@unizar.es>
2023-05-23 20:55:41 +0200

    Skip new test on Windows

--- testsuite/tests/install/softlinks/test.py
+++ testsuite/tests/install/softlinks/test.py
@@ -2,7 +2,16 @@
 Test that binary files containing softlinks can be installed properly
 """
 
+import sys
+
 from drivers.alr import run_alr
+from drivers.helpers import on_windows
+
+
+# Does not apply to Windows as it does not support softlinks
+if on_windows():
+    print('SKIP: on Windows, unapplicable')
+    sys.exit(0)
 
 # This command should succeed normally
 run_alr("install", "--prefix=install", "crate")
