6a3a818a88d3f9f9a3bdfa10468e4f1f0e707a45
Alejandro R. Mosteo <amosteo@unizar.es>
2023-05-30 20:50:44 +0200

    Fix test of implicit external sharing

--- testsuite/tests/with/shared-switch-external/test.py
+++ testsuite/tests/with/shared-switch-external/test.py
@@ -1,5 +1,6 @@
 """
-Check that selective sharing doesn't affect an external crate
+Check that selective sharing doesn't affect an external crate, which is always
+implicitly shared.
 """
 
 import os
@@ -8,12 +9,24 @@ import shutil
 from drivers.alr import init_local_crate, run_alr
 from drivers.asserts import assert_match, match_deploy_dir
 
-# Add an external crate, requesting it to be shared
+# Add and external crate, without requesting sharing
 init_local_crate()
-run_alr("with", "--shared", "gnat_external")
+run_alr("with", "gnat_external")
 
-# Ensure that the shared flag is not in the solution
+# Check that the shared flag is in the solution nonetheless
 p = run_alr("with", "--solve")
-assert_match(".*gnat_external=[^\\n]* \(origin: external\)", p.out)
+assert_match(".*gnat_external=[^\\n]* \(shared\) \(origin: external\)", p.out)
+
+
+# Start afresh in new crate
+init_local_crate()
+
+# Add an external crate, requesting it not to be shared
+run_alr("with", "--shared=no_local", "gnat_external")
+
+# Check that the shared flag is in the solution nonetheless
+p = run_alr("with", "--solve")
+assert_match(".*gnat_external=[^\\n]* \(shared\) \(origin: external\)", p.out)
+
 
 print('SUCCESS')
