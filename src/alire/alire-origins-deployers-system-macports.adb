fa6c67da824c8d4d44f0d78da9a3f63e373324f7
Simon Wright <simon@pushface.org>
2023-07-18 12:37:59 +0100

    Support MacPorts as external provider. (#1404)
    * src/alire/alire-origins-deployers-system-macports.adb: new.
      * src/alire/alire-origins-deployers-system-macports.ads: new.
      * src/alire/alire-origins-deployers-system.adb
        (context): add Alire.Origins.Deployers.System.Macports.
        (Platform_Deployer): Platforms.Macports => System.Macports.Deployer.
      * src/alire/alire-platforms.ads
        (Distributions): add Macports.
        (Package_Managers): likewise.
        (Distro_Manager): Macports => Macports.
      * src/alire/alire-utils-tools.adb (System_Package_For_Tool): add Macports,
          same as the other distributions.
      * src/alire/os_macos/alire-platforms-current__macos.adb:
        (context): add GNAT.OS_Lib.
        (Port_Access): new, access to Macports' 'port' executable.
        (Macports_Present): new, true if 'port' was found.
        (Detected_Distribution): first, check for Homebrew_Present, then, for
          Macports_Present. Return the corresponding Distribution if found.

---
+++ src/alire/alire-origins-deployers-system-macports.adb
@@ -0,0 +1,114 @@
+with AAA.Strings; use AAA.Strings;
+
+with Alire.OS_Lib.Subprocess;
+with Alire.Errors;
+
+package body Alire.Origins.Deployers.System.Macports is
+
+   --  Ada.Strings.Unbounded is use-visible via Alire.Origins.
+
+   package Subprocess renames Alire.OS_Lib.Subprocess;
+
+   -----------------------
+   -- Already_Installed --
+   -----------------------
+
+   overriding function Already_Installed (This : Deployer) return Boolean
+   is
+      Installed : AAA.Strings.Vector;
+   begin
+      Trace.Debug ("already_installed? " & This.Base.Package_Name);
+
+      if Subprocess.Unchecked_Spawn_And_Capture
+        ("port",
+         Empty_Vector & "echo" & "installed",
+         Output     => Installed,
+         Err_To_Out => True) /= 0
+      then
+         --  failed.
+         Trace.Debug ("port failed to find any installed packages");
+         return False;
+      end if;
+
+      --  We get a list of all the installed packages,
+      --  name {spaces} '@' version
+
+      Trace.Debug (Installed.Length'Image & " installed packages");
+
+      for P of Installed loop
+         Trace.Debug ("Checking '" & P & "'");
+         if AAA.Strings.Head (P, ' ') = This.Base.Package_Name then
+            Trace.Debug ("found '" & P & "'");
+            return True;
+         end if;
+      end loop;
+
+      return False;  -- until we've implemented Install
+   end Already_Installed;
+
+   ------------
+   -- Detect --
+   ------------
+
+   --  Returns the package version, if the package exists, whether or
+   --  not it's installed.
+   --
+   --  If it's not installed, what you get is the version that would
+   --  be installed.
+
+   overriding
+   function Detect (This : Deployer) return Version_Outcomes.Outcome
+   is
+      Info : AAA.Strings.Vector;
+   begin
+      Trace.Debug ("detect? " & This.Base.Package_Name);
+
+      if Subprocess.Unchecked_Spawn_And_Capture
+        ("port",
+         Empty_Vector & "info" & "--version" & This.Base.Package_Name,
+         Output     => Info,
+         Err_To_Out => True) /= 0
+      then
+         --  failed.
+         Trace.Debug ("port failed to find " & This.Base.Package_Name);
+         return Version_Outcomes.Outcome_Failure
+           ("no candidate version found",
+            Report => False);
+      end if;
+
+      if Integer (Info.Length) /= 1 then
+         raise Constraint_Error
+           with "port info --version returned" & Info.Length'Image & " lines.";
+      end if;
+
+      Trace.Debug ("port info output: " & Info (Info.First));
+      declare
+         Version : constant String := Trim (Tail (Info (Info.First), ':'));
+      begin
+         Trace.Debug (" -> version: '" & Version & "'");
+         return Version_Outcomes.New_Result (Semantic_Versioning.Parse
+                                               (Version,
+                                                Relaxed => True));
+      end;
+
+   end Detect;
+
+   -------------
+   -- Install --
+   -------------
+
+   overriding
+   function Install (This : Deployer) return Outcome is
+   begin
+      Trace.Debug ("hoping to install: " & This.Base.Image);
+      Subprocess.Checked_Spawn
+        ("sudo",
+         Empty_Vector & "port" & "install" & This.Base.Package_Name);
+
+      return Outcome_Success;
+   exception
+      when E : others =>
+         return Alire.Errors.Get (E);
+   end Install;
+
+end Alire.Origins.Deployers.System.Macports;
