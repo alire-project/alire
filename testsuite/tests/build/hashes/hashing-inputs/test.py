03f7a7f5ecc1a410a952bc8fb165fb280d0bca61
Alejandro R Mosteo <amosteo@unizar.es>
2023-09-04 12:02:37 +0200

    Add crate environment to build hash inputs (#1432)
    * Add crate-modified env vars to hash input
    
    * Test environment hashing

--- testsuite/tests/build/hashes/hashing-inputs/test.py
+++ testsuite/tests/build/hashes/hashing-inputs/test.py
@@ -1,11 +1,12 @@
 """
-Test that the inputs to the hashing properly reflect the build profile
+Test that the inputs to the hashing properly reflect the build profile and
+other inputs.
 """
 
 import shutil
-from drivers.alr import alr_with, init_local_crate, run_alr
+from drivers.alr import alr_with, external_compiler_version, init_local_crate, run_alr
 from drivers.builds import find_hash, hash_input
-from drivers.asserts import assert_match
+from drivers.asserts import assert_eq, assert_match
 from drivers import builds
 
 run_alr("config", "--set", "--global", "dependencies.shared", "true")
@@ -29,15 +30,18 @@ assert_match(".*profile:libhello=VALIDATION.*",
 # Check that the hashes are different
 assert hash1 != hash2, "Hashes should be different"
 
-# Chech that the hash inputs contains GPR externals
-# either set or observed by the crate:
+# Chech that the hash inputs contains exactly what we expect it to contain.
+# This includes environment variables, GPR externals set or observed, build
+# profile, compiler version.
 
-assert_match(
-    ".*external:TEST_FREEFORM_UNSET=default.*"  # a declared unset external
-    ".*external:TEST_GPR_EXTERNAL=gpr_ext_B.*"  # a declared & set enum external
-    ".*external:TEST_UNDECLARED=used_by_another_crate.*",
-                                                # set without prev declaration
+assert_eq(
+    'environment:TEST_ENV=myenv\n'           # plain env var set
+    'external:TEST_FREEFORM_UNSET=default\n' # declared unset GPR external
+    'external:TEST_GPR_EXTERNAL=gpr_ext_B\n' # declared set GPR external
+    'external:TEST_UNDECLARED=used_by_another_crate\n' # modified GPR external
+    'profile:libhello=VALIDATION\n'          # build profile
+    f'version:gnat_external={external_compiler_version()}\n',
+                                             # compiler version
     hash_input("libhello"))
-
 
 print("SUCCESS")
