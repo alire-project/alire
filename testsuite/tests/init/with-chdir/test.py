cba2c11267b2a5e2c550469735f1b93f67c66321
tali auster <taliauster@gmail.com>
2023-10-26 11:52:36 -0600

    clean up with-chdir test

--- testsuite/tests/init/with-chdir/test.py
+++ testsuite/tests/init/with-chdir/test.py
@@ -1,20 +1,22 @@
 """
-Test "executable" only appears in --bin initializations
+Test --chdir switch.
 """
 
-import os.path
+import os
 
 from drivers.alr import run_alr
 from drivers.asserts import assert_match
-from drivers.helpers import content_of
 
 test_dir = os.getcwd()
 
-# Binary crate
+# Make a binary crate in test that it runs without cd'ing.
 run_alr("init", "--bin", "xxx")
-assert_match(".*executables = \[", content_of("xxx/alire.toml"))
-
-# Check that it builds and runs
 run_alr("--chdir=xxx", "run")
+
+# Test that changing to a non-existent directory fails. Technically,
+# /dev/null/cantexist can exist, but it's more than reasonable to assume a
+# system with this directory has bigger issues.
+bad_chdir = run_alr("-C", "/dev/null/cantexist", "run", complain_on_error=False)
+assert_match(".*directory \"/dev/null/cantexist\" does not exist.*", bad_chdir.out)
 
 print('SUCCESS')
