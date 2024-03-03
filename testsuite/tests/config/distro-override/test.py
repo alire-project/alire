acb8cb2da970d08fdf2c9a48600aa8d6175b102b
Alejandro R. Mosteo <amosteo@unizar.es>
2024-03-03 20:41:54 +0100

    Override distro via config key

---
+++ testsuite/tests/config/distro-override/test.py
@@ -0,0 +1,23 @@
+"""
+Verify that distro overridding works as intended
+"""
+
+from drivers.alr import run_alr, distro_is_known
+from drivers.asserts import assert_match
+
+# Overriding distro detection. We force Debian as our tests run in Ubuntu and
+# many other distros so it is only giving a false positive on Debian.
+run_alr("config", "--global",
+        "--set", "distribution.override", "debian")
+
+assert_match(".*distribution:[^\n]*DEBIAN",
+             run_alr("version").out)
+
+# Disabling distro detection takes precedence
+run_alr("config", "--global",
+        "--set", "distribution.disable_detection", "true")
+
+assert_match(".*distribution:[^\n]*DISTRIBUTION_UNKNOWN",
+             run_alr("version").out)
+
+print('SUCCESS')
