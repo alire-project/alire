bc2fb0137e2b67f86dac2cf14247308497a76659
Alejandro R Mosteo <amosteo@unizar.es>
2024-09-03 15:33:17 +0200

    Fix early error on Windows with msys2 disabled (#1747)
    * Fix non-absolute path returned when msys2 disabled
    
    * Add test for the fix
    
    * Wrong platform build friendlier message

--- testsuite/tests/settings/distro-disable/test.py
+++ testsuite/tests/settings/distro-disable/test.py
@@ -2,11 +2,24 @@
 Verify that disabling distro detection works as intended
 """
 
-from drivers.alr import run_alr, distro_is_known
+from drivers.alr import run_alr, distro_is_known, init_local_crate, alr_with, alr_manifest
 
 run_alr("settings", "--global",
         "--set", "distribution.disable_detection", "true")
 
+# Inspect output of `alr version`
 assert not distro_is_known(), "Unexpected distro detection"
+
+# Ensure that basic environment can be printed for crates that use
+# $DISTRIB_ROOT
+
+init_local_crate()
+# Append usage of DISTRIB_ROOT to the manifest
+with open(alr_manifest(), "a") as f:
+    f.write("""
+    [environment]
+    PATH.append = "${DISTRIB_ROOT}"
+    """)
+run_alr("printenv")
 
 print('SUCCESS')
