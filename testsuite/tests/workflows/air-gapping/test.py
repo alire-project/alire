e44265e94d87b1903f4be004f2980c579aae9308
Seb M'Caw <mcaw@adacore.com>
2024-09-23 09:38:36 +0000

    Change crates from git repos to .tgz archives

--- testsuite/tests/workflows/air-gapping/test.py
+++ testsuite/tests/workflows/air-gapping/test.py
@@ -11,10 +11,10 @@ from drivers.alr import run_alr
 from drivers.asserts import assert_eq, assert_match
 
 
-# Mock git, curl, gprbuild etc. with dummy scripts
+# Mock tar, git, curl, gprbuild etc. with dummy scripts
 os.mkdir("path-dir")
 os.chdir("path-dir")
-for executable in ("git", "hg", "svn", "curl", "gprbuild"):
+for executable in ("tar", "git", "hg", "svn", "curl", "gprbuild"):
     with open(executable, "w") as f:
         f.write("\n".join([
             "#!/usr/bin/env python",
@@ -27,21 +27,21 @@ os.environ["PATH"] = f'{os.getcwd()}{os.pathsep}{os.environ["PATH"]}'
 os.chdir("..")
 
 
-# Run `alr get hello`. This will fail because git is unavailable.
+# Run `alr get hello`. This will fail because tar is unavailable.
 p = run_alr("get", "hello", quiet=False, complain_on_error=False)
 assert_match(".*Mocked command called", p.out)
-assert_match(".*Deployment of commit .* from .* to .* failed", p.out)
+assert_match(".*Deployment of path .* to .* failed", p.out)
 
-# Disable git mocking and run `alr get hello` to 'download' the crate and its
+# Disable tar mocking and run `alr get hello` to 'download' the crate and its
 # dependencies.
-os.remove(os.path.join("path-dir", "git"))
+os.remove(os.path.join("path-dir", "tar"))
 p = run_alr("get", "hello", quiet=False)
 assert_match(r".*hello=1\.0\.1 successfully retrieved", p.out)
 assert_match(r".*\+ libhello 1\.0\.0 \(new\)", p.out)
 
-# Re-enable git mocking and make the index unavailable to simulate disconnection
+# Re-enable tar mocking and make the index unavailable to simulate disconnection
 # from the network
-shutil.copy(os.path.join("path-dir", "curl"), os.path.join("path-dir", "git"))
+shutil.copy(os.path.join("path-dir", "curl"), os.path.join("path-dir", "tar"))
 shutil.move("my_index", "somewhere_else")
 
 # Simulate transferring to a different system by clearing the alr-config
@@ -50,7 +50,7 @@ shutil.move("my_index", "somewhere_else")
 for f in os.listdir("alr-config"):
     if f != "settings.toml":
         shutil.rmtree(os.path.join("alr-config", f))
-shutil.move(f"hello_1.0.1_cd89b04b", "hello")
+shutil.move(f"hello_1.0.1_filesystem", "hello")
 
 # Run `alr build`. This will fail because gprbuild is unavailable.
 os.chdir(f"hello")
@@ -58,7 +58,7 @@ p = run_alr("build", quiet=False, complain_on_error=False)
 assert_match(".*Mocked command called", p.out)
 assert_match(r'.*Command \["gprbuild", .*\] exited with code 1', p.out)
 
-# Disable gprbuild mocking and run `alr build` to build the crate (with git
+# Disable gprbuild mocking and run `alr build` to build the crate (with tar
 # mocking still enabled to check it doesn't try to fetch anything else)
 os.remove(os.path.join("..", "path-dir", "gprbuild"))
 p = run_alr("build", quiet=False)
@@ -73,8 +73,11 @@ assert_eq(
 # attempts (and fails) to re-fetch them
 shutil.rmtree(os.path.join("alire", "cache", "dependencies"))
 p = run_alr("build", quiet=False, complain_on_error=False)
-assert_match(".*Mocked command called", p.out)
-assert_match(".*Deployment of commit .* from .* to .* failed", p.out)
+assert_match(
+    ".*Filesystem crate is neither a folder nor a source archive: ",
+    p.out
+)
+assert_match(".*Deployment of path .* to .* failed", p.out)
 
 
 print("SUCCESS")
