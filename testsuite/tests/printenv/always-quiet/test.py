cd9ef7b5b96089092ceaea7d0495a0f8aa9a8607
Alejandro R. Mosteo <amosteo@unizar.es>
2025-02-25 09:37:22 +0100

    Simplify test

--- testsuite/tests/printenv/always-quiet/test.py
+++ testsuite/tests/printenv/always-quiet/test.py
@@ -6,7 +6,13 @@ sync/update needed.
 import os
 import shutil
 from drivers.alr import alr_lockfile, alr_with, init_local_crate, run_alr, run_alr_interactive
-from drivers.asserts import assert_eq, assert_match
+from drivers.asserts import assert_eq
+
+
+def check_output(output : str):
+    # Split in lines and verify that every line is an export    
+    for line in output.splitlines():
+        assert line.startswith("export ")
 
 
 #  We create a new crate with some dependencies and delete the `alire` forder.
@@ -30,18 +36,9 @@ p2 = run_alr("printenv", "--unix", quiet=True)
 # Output should match
 assert_eq(p1.out, p2.out)
 
-# Also compare to ground truth (specific paths are not important, "export" variables are)
-assert_match("""\
-export ALIRE="True"
-export GPR_PROJECT_PATH=".*"
-export HELLO_ALIRE_PREFIX="/.*/printenv__always-quiet/.*/hello_1.0.1_filesystem.*"
-export LIBHELLO_ALIRE_PREFIX="/.*/printenv__always-quiet/.*/libhello_1.0.0_filesystem.*"
-export TEST_ENV="myenv"
-export TEST_GPR_EXTERNAL="gpr_ext_B"
-export TEST_UNDECLARED="used_by_another_crate"
-export XXX_ALIRE_PREFIX="/.*/printenv__always-quiet/xxx"
-""",             
-             p1.out)
+# Also check that every line is an export and not something else.
+# We do not check the specific contents as they vary between OSes and build modes.
+check_output(p1.out)
 
 # Test that a non-interactive run also completes without trying to interact or
 # with unexpected output
