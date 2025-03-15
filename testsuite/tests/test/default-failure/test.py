5878af87ec4e9ff6e745a977f7864c66063bd9c7
Alejandro R Mosteo <amosteo@unizar.es>
2025-03-16 00:34:40 +0100

    fix: enable assertions in test crate (#1902)

--- testsuite/tests/test/default-failure/test.py
+++ testsuite/tests/test/default-failure/test.py
@@ -9,10 +9,24 @@ from drivers.asserts import assert_match
 
 init_local_crate("xxx", with_test=True)
 
+# Check with explicit exception
+
 with open("./tests/src/xxx_tests-example_test.adb", "w") as f:
    f.write("""procedure Xxx_Tests.Example_Test is
 begin
    raise Program_Error;
+end Xxx_Tests.Example_Test;
+""")
+
+p = run_alr("test", complain_on_error=False)
+assert_match(".*\[ FAIL \] example_test.*", p.out)
+
+# Check with plain assertion (verify that assertions are evaluated)
+
+with open("./tests/src/xxx_tests-example_test.adb", "w") as f:
+   f.write("""procedure Xxx_Tests.Example_Test is
+begin
+   pragma Assert (False);
 end Xxx_Tests.Example_Test;
 """)
 
