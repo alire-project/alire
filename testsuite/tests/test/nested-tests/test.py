f5a0f62007cd71f6265632a78cb18c273248c5e0
Alejandro R. Mosteo <amosteo@unizar.es>
2025-03-19 20:54:36 +0100

    Improved test

--- testsuite/tests/test/nested-tests/test.py
+++ testsuite/tests/test/nested-tests/test.py
@@ -3,6 +3,8 @@ Check that tests deeper inside "src" subfolders are found and tested correctly,
 and the name printed keeps the prefix inside "src".
 """
 
+from os import makedirs
+
 from drivers.alr import init_local_crate, run_alr
 from drivers.asserts import assert_substring
 from drivers.helpers import mkcd
@@ -11,12 +13,9 @@ from drivers.helpers import mkcd
 init_local_crate("xxx", with_test=True)
 
 # Create a second test inside the "tests/src/nested" folder
-from os import makedirs
 
-# Create the nested directory structure
 makedirs("tests/src/nested", exist_ok=True)
 
-# Create a test file in the nested directory
 with open("tests/src/nested/xxx_tests-nested_test.adb", "w") as f:
     f.write("""
 procedure Xxx_Tests.Nested_Test is
@@ -26,8 +25,28 @@ end Xxx_Tests.Nested_Test;
 """)
 
 # Run `alr test` and check that two PASS lines exist with the proper test names
+
 p = run_alr("test")
 assert_substring("[ PASS ] example_test", p.out)
 assert_substring("[ PASS ] nested/nested_test", p.out)
+
+# Create a third failing test to check its failure and output are reported
+
+with open("tests/src/nested/xxx_tests-failing_test.adb", "w") as f:
+    f.write("""
+procedure Xxx_Tests.Failing_Test is
+begin
+   raise Program_Error;
+end Xxx_Tests.Failing_Test;
+""")
+
+p = run_alr("test", complain_on_error=False)
+assert_substring("""\
+[ FAIL ] nested/failing_test
+*** Test output ***
+
+raised PROGRAM_ERROR : xxx_tests-failing_test.adb""",
+    p.out
+)
 
 print("SUCCESS")
