c3139cc4cbc4e8bbe8b0d1e82f2763b879f2022b
Alejandro R. Mosteo <amosteo@unizar.es>
2025-07-10 14:08:00 +0200

    Regenerate templates

--- src/templates/r.adb
+++ src/templates/r.adb
@@ -34,60 +34,60 @@ package body r is
          Register
             ("crate_bin/alire.toml",
              r.crate_bin_alire_toml.Content'Access,
-             GNAT.Calendar.Time_Of (2025, 06, 09, 14, 52, 10, 0.0));
+             GNAT.Calendar.Time_Of (2025, 04, 29, 05, 06, 37, 0.0));
          Register
             ("crate_bin/name.gpr",
              r.crate_bin_name_gpr.Content'Access,
-             GNAT.Calendar.Time_Of (2025, 06, 09, 14, 52, 10, 0.0));
+             GNAT.Calendar.Time_Of (2025, 04, 29, 05, 06, 37, 0.0));
          Register
             ("crate_bin/src/name.adb",
              r.crate_bin_src_name_adb.Content'Access,
-             GNAT.Calendar.Time_Of (2025, 06, 09, 14, 52, 10, 0.0));
+             GNAT.Calendar.Time_Of (2025, 04, 29, 05, 06, 37, 0.0));
          Register
             ("crate_common/gitignore.hidden",
              r.crate_common_gitignore_hidden.Content'Access,
-             GNAT.Calendar.Time_Of (2025, 06, 09, 14, 52, 10, 0.0));
+             GNAT.Calendar.Time_Of (2025, 04, 29, 05, 06, 37, 0.0));
          Register
             ("crate_lib/alire.toml",
              r.crate_lib_alire_toml.Content'Access,
-             GNAT.Calendar.Time_Of (2025, 06, 09, 14, 52, 10, 0.0));
+             GNAT.Calendar.Time_Of (2025, 04, 29, 05, 06, 37, 0.0));
          Register
             ("crate_lib/name.gpr",
              r.crate_lib_name_gpr.Content'Access,
-             GNAT.Calendar.Time_Of (2025, 06, 09, 14, 52, 10, 0.0));
+             GNAT.Calendar.Time_Of (2025, 04, 29, 05, 06, 37, 0.0));
          Register
             ("crate_lib/src/name.ads",
              r.crate_lib_src_name_ads.Content'Access,
-             GNAT.Calendar.Time_Of (2025, 06, 09, 14, 52, 10, 0.0));
+             GNAT.Calendar.Time_Of (2025, 04, 29, 05, 06, 37, 0.0));
          Register
             ("crate_test/tests/alire.toml",
              r.crate_test_tests_alire_toml.Content'Access,
-             GNAT.Calendar.Time_Of (2025, 06, 09, 14, 52, 10, 0.0));
+             GNAT.Calendar.Time_Of (2025, 04, 29, 05, 06, 37, 0.0));
          Register
             ("crate_test/tests/common/name_tests.ads",
              r.crate_test_tests_common_name_tests_ads.Content'Access,
-             GNAT.Calendar.Time_Of (2025, 06, 09, 14, 52, 10, 0.0));
+             GNAT.Calendar.Time_Of (2025, 04, 29, 05, 06, 37, 0.0));
          Register
             ("crate_test/tests/crate_test_tests.gpr",
              r.crate_test_tests_crate_test_tests_gpr.Content'Access,
-             GNAT.Calendar.Time_Of (2025, 06, 09, 14, 52, 10, 0.0));
+             GNAT.Calendar.Time_Of (2025, 04, 29, 05, 06, 37, 0.0));
          Register
             ("crate_test/tests/src/name_tests-assertions_enabled."
              & "adb",
              r.crate_test_tests_src_name_testsxassertions_enabled_adb.Content'Access,
-             GNAT.Calendar.Time_Of (2025, 06, 09, 14, 52, 10, 0.0));
+             GNAT.Calendar.Time_Of (2025, 04, 29, 05, 06, 37, 0.0));
          Register
             ("github/README.md",
              r.github_readme_md.Content'Access,
-             GNAT.Calendar.Time_Of (2025, 07, 03, 15, 30, 24, 0.0));
+             GNAT.Calendar.Time_Of (2025, 07, 10, 12, 00, 08, 0.0));
          Register
             ("github/publish.yml",
              r.github_publish_yml.Content'Access,
-             GNAT.Calendar.Time_Of (2025, 07, 03, 18, 25, 39, 0.0));
+             GNAT.Calendar.Time_Of (2025, 07, 10, 12, 00, 27, 0.0));
          Register
             ("github/selftest.yml",
              r.github_selftest_yml.Content'Access,
-             GNAT.Calendar.Time_Of (2025, 07, 03, 18, 27, 47, 0.0));
+             GNAT.Calendar.Time_Of (2025, 07, 10, 12, 06, 00, 0.0));
       end if;
    end Init;
 
