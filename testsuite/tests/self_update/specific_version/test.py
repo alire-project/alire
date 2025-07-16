210c4f8b6fa0469a37822c59ddf9f13d72e7ed79
AldanTanneo <aldantanneo@gmail.com>
2025-07-16 17:33:59 +0200

    try to stabilize tests on windows

--- testsuite/tests/self_update/specific_version/test.py
+++ testsuite/tests/self_update/specific_version/test.py
@@ -11,6 +11,7 @@ import os
 v_init = drivers.alr.run_alr("version").out
 
 shutil.copy(os.environ["ALR_PATH"], ".")
+local_alr = f".{os.sep}{exe_name('alr')}"
 
 curl_script = """\
 import os
@@ -20,19 +21,14 @@ import sys
 env2 = os.environ.copy()
 env2["PATH"] = env2["PATH"].split(os.pathsep, 1)[1]
 token_header = []
-if "GITHUB_TOKEN" in os.environ and any(
-    a.startswith("https://api.github.com") for a in sys.argv[1:]
-):
+if "GITHUB_TOKEN" in os.environ:
     token_header = ["-H", f"Authorization: Bearer {os.environ['GITHUB_TOKEN']}"]
 subprocess.call(["curl", *token_header, *sys.argv[1:]], env=env2)
 """
 
 
 def run_alr(args: list[str], expect_success: bool = True) -> str:
-    p = run(
-        [f".{os.sep}{exe_name('alr')}", "-n", *args],
-        capture_output=True,
-    )
+    p = run([local_alr, "-n", *args], capture_output=True)
     assert expect_success == (
         p.returncode == 0
     ), f"""stdout: {p.stdout.decode(errors="replace")}
@@ -41,6 +37,7 @@ stderr: {p.stderr.decode(errors="replace")}"""
 
 
 with MockCommand("curl", curl_script, "curl_override"):
+    initial_mtime = os.path.getmtime(local_alr)
     out = run_alr(["self-update", "--release=2.1.0"])
 
     if (
@@ -50,16 +47,16 @@ with MockCommand("curl", curl_script, "curl_override"):
         print("SUCCESS")
         __import__("sys").exit()
 
-    out = None
-    for i in range(10):
+    for _ in range(10):
+        time.sleep(1)
         # on windows, the self-update process runs detached,
         # so we run this in a loop until it's done (or failed)
-        out = run_alr(["--version"])
-        if "2.1" in out:
+        if os.path.getmtime(local_alr) != initial_mtime:
+            # detect changes by getting the file's modification time
+            time.sleep(0.25)  # ongoing copy?
             break
-        time.sleep(1)
 
-assert_substring("2.1", out)
+assert_substring("2.1", run_alr(["--version"]))
 
 assert drivers.alr.run_alr("version").out == v_init  # ensure the main alr is unchanged
 
