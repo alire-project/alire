bdea4b0fdb07119381705de9b41fe383430d3941
Luke A. Guest <laguest@archeia.com>
2025-09-27 20:16:44 +0100

    Fix indexing error. Check to make sure we have a line to check before checking the line.

--- src/alire/alire-origins-deployers-system-portage.adb
+++ src/alire/alire-origins-deployers-system-portage.adb
@@ -33,7 +33,12 @@ package body Alire.Origins.Deployers.System.Portage is
    begin
       Trace.Info ("Already_Installed: " & This.Base.Package_Name);
 
-      return Line (Line'First .. Line'First + Indicator'Length) = Indicator;
+      if Line'Length >= Indicator'Length then
+         return
+           Line (Line'First .. Line'First + Indicator'Length - 1) = Indicator;
+      end if;
+
+      return False;
    end Already_Installed;
 
    function To_Semantic_Version (Gentoo_Version : String) return String is
