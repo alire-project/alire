7c172fc6a755c72d3efe83ba0952acbe1d52f682
Alejandro R. Mosteo <amosteo@unizar.es>
2025-10-03 18:18:44 +0200

    fix: exit with code 0 to prevent spurious blank line

--- src/alr/alr-main.adb
+++ src/alr/alr-main.adb
@@ -1,7 +1,9 @@
 with Ada.Exceptions;
 
+with Alire.Directories;
 with Alire_Early_Elaboration; pragma Elaborate_All (Alire_Early_Elaboration);
 with Alire.Errors;
+with Alire.OS_Lib;
 
 with Alr.Commands;
 with Alr.Last_Chance_Handler;
@@ -11,6 +13,28 @@ begin
    Trace.Debug ("alr platform configured");
 
    Commands.Execute;
+
+   --  If we let the program end by its own means, there's a spurious blank
+   --  line printed sometimes. Debugging traces it to a call to
+   --
+   --  procedure s_stalib_adafinal;
+   --  pragma Import (Ada, s_stalib_adafinal,
+   --                 "system__standard_library__adafinal");
+   --
+   --  in the generated b__alr-main.adb
+   --
+   --  Since this doesn't always happen, it might be some thing we're doing in
+   --  those cases that comes back at finalization time. Cursory check of our
+   --  Finalize procedures didn't turn anything up. That call cannot be stepped
+   --  into, which difficults things. To be investigated...
+   --
+   --  Sample trigger: `alr exec echo whatever`
+
+   Alire.Directories.Delete_Temporaries;
+   --  Should not be needed as any temporary is now out of scope, but just in
+   --  case we call as if we were ending prematurely.
+
+   Alire.OS_Lib.Bailout (0); -- See previous paragraph
 exception
    when E : Program_Error =>
       Alire.Log_Exception (E);
