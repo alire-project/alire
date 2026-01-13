e4254336461c261af2404a2130221703a60b6e8b
Alejandro R. Mosteo <amosteo@unizar.es>
2026-01-13 18:18:02 +0100

    dev: double default solver timeout to 10s

--- src/alire/alire-settings-builtins.ads
+++ src/alire/alire-settings-builtins.ads
@@ -159,7 +159,7 @@ package Alire.Settings.Builtins is
    Solver_Timeout : constant Builtin := New_Builtin
      (Key    => "solver.timeout",
       Kind   => Stn_Int,
-      Def    => "5",
+      Def    => "10",
       Help   => "Seconds until solver first timeout (-1 to disable)");
 
    Solver_Grace_Period : constant Builtin := New_Builtin
