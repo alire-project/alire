51cd370a9eba0563b768bd721eaa78bed4abf49d
AldanTanneo <aldantanneo@gmail.com>
2026-01-27 18:04:15 +0100

    make testsuite accept timings in test output

--- testsuite/tests/test/conditional-tests/test.py
+++ testsuite/tests/test/conditional-tests/test.py
@@ -7,7 +7,7 @@ import os
 import shutil
 from drivers.alr import alr_manifest, init_local_crate, init_local_crate, run_alr
 from drivers.helpers import append_to_file
-from drivers.asserts import assert_substring
+from drivers.asserts import assert_substring, assert_match
 
 CRATE="xxx"
 
@@ -31,7 +31,7 @@ for mode in ["table", "array"]:
 
     # Verify the test runs in all platforms
     p = run_alr("test")
-    assert_substring("[ PASS ] tests", p.out)
+    assert_match(".*\[ PASS \] *\d+[smh]\d+ tests.*", p.out)
 
     # Verify the crate can be shown after resolving conditional expressions (this is
     # equivalent to exporting the crate). `--system` is the key flag to force evaluation.
--- testsuite/tests/test/suppress-output/test.py
+++ testsuite/tests/test/suppress-output/test.py
@@ -3,7 +3,7 @@ Run a failing test and check its output is suppressed when using -q
 """
 
 from drivers.alr import init_local_crate, run_alr
-from drivers.asserts import assert_substring
+from drivers.asserts import assert_substring, assert_match
 
 init_local_crate("xxx", with_test=True)
 
@@ -16,12 +16,12 @@ end Xxx_Tests.Assertions_Enabled;
 
 # check that when -q is set to false, the failing test output is displayed
 p = run_alr("test", complain_on_error=False, quiet=False)
-assert_substring("[ FAIL ] assertions_enabled", p.out)
+assert_match(".*\[ FAIL \] *\d+[smh]\d+ assertions_enabled.*", p.out)
 assert_substring("raised PROGRAM_ERROR : xxx_tests-assertions_enabled.adb", p.out)
 
 # check that when -q is passed, the failing test output is suppressed
 p = run_alr("test", complain_on_error=False, quiet=True)
-assert_substring("[ FAIL ] assertions_enabled", p.out)
+assert_match(".*\[ FAIL \] *\d+[smh]\d+ assertions_enabled.*", p.out)
 assert "raised PROGRAM_ERROR : xxx_tests-assertions_enabled.adb" not in p.out
 
 print("SUCCESS")
