3d944003c7b3482f20146ff0121b312491051f5b
César Sagaert <aldantanneo@gmail.com>
2026-02-06 00:12:04 +0100

    feat: make alr test search for a test runner in parent dirs (#2076)

--- testsuite/tests/test/from-subdir/test.py
+++ testsuite/tests/test/from-subdir/test.py
@@ -1,6 +1,5 @@
 """
-Test that running `alr test` from a subdirectory other than the root and the
-`tests` directory works as expected.
+Test that running `alr test` from a subdirectory works as expected.
 """
 
 from drivers.alr import init_local_crate, run_alr
@@ -19,5 +18,16 @@ assert_substring("[ PASS ]", p.out)
 mkcd("subsubdir")
 p = run_alr("test")
 assert_substring("[ PASS ]", p.out)
+
+# create a subcrate two levels down without a `test` section
+init_local_crate("yyy", with_test=False)
+p = run_alr("test")
+assert_substring("[ PASS ]", p.out)
+
+# check that using `--legacy` does not run the testsuite from
+# the toplevel crate
+p = run_alr("test", "--legacy", quiet=False)
+assert "[ PASS ]" not in p.out
+assert_substring("Successful actions run", p.out)
 
 print("SUCCESS")
