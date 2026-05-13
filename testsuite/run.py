467a0a64c2ae35eec5ba38cb74d3a7c11f062a93
Alejandro R. Mosteo <amosteo@unizar.es>
2026-05-13 17:46:24 +0200

    Report alr in use by the testsuite

--- testsuite/run.py
+++ testsuite/run.py
@@ -63,6 +63,13 @@ class Testsuite(e3.testsuite.Testsuite):
         # during the tests (e.g. submitting a release by accident)
         os.environ["ALR_TESTSUITE"] = "TRUE"
 
+        # Report alr being used
+        alr_path = self.main.args.alr_path
+        print(f"Testsuite using alr at {alr_path} with version:")
+        print(subprocess.run([alr_path, 'version'],
+                             stdout=subprocess.PIPE).stdout.decode())
+        sys.stdout.flush()
+
         # Ensure toolchain is in scope, or err early instead of during tests
         # Locate gnat and gprbuild in path and report their location
         required_executables = ['gnat', 'gprbuild']
