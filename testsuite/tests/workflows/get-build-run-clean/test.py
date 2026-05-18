6a9cb72dde6c9d91c12244aa3b79adfd3b21c658
Alejandro R. Mosteo <amosteo@unizar.es>
2026-05-18 17:06:19 +0200

    Bypass known leftovers from gprclean

--- testsuite/tests/workflows/get-build-run-clean/test.py
+++ testsuite/tests/workflows/get-build-run-clean/test.py
@@ -7,6 +7,7 @@ import os
 
 from drivers.alr import run_alr
 from drivers.asserts import assert_eq
+from drivers.helpers import contents
 
 
 # Get the "hello" project and enter its directory
@@ -23,7 +24,20 @@ assert_eq('Hello, world!\n', p.out)
 # Clean it
 assert os.listdir('obj')
 run_alr('clean')
-assert not os.listdir('obj'), \
-    "Expected empty obj/ but contents are:" + str(os.listdir('obj'))
+
+# NOTE: gprbuild/gprclean 26.0 leaves behind files like:
+# obj/development/xxx.adb.stderr
+# obj/development/xxx.adb.stdout
+# obj/development/xxx.bexch
+# obj/development/xxx.cswi
+# This is likely a bug in gprclean, so instead of checking that obj/ is empty,
+# as we did before, we check that remaining files have one of those extensions.
+
+GPRCLEAN_BUG_EXTENSIONS = {'.stderr', '.stdout', '.bexch', '.cswi'}
+spurious = [f for f in contents('obj')
+            if os.path.isfile(f)
+            and os.path.splitext(f)[1] not in GPRCLEAN_BUG_EXTENSIONS]
+assert not spurious, \
+    "Unexpected files after clean: " + str(spurious)
 
 print('SUCCESS')
